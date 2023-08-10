2448375d8ad81fac530ea110697a463e2fd48c17
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-10 15:41:10 -0700

    Add button component

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         Admin Dashboard
+        <Button>Click Me!</Button>
       </div>
     </main>
   )
