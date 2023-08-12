0918188a77e3737c3d73afef723543599da50d23
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-12 15:26:15 -0700

    Use <UserButton />
    Complete full authentication circle by using Clerk <UserButton /> component that allows user to manage account information and log out.

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function SetupPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Admin Dashboard
-        <Button>Click Me!</Button>
+        <UserButton afterSignOutUrl="/"/>
       </div>
     </main>
   )
