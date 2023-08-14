2fce41d689339fda345fc452ead661b7d964809a
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-13 18:23:01 -0700

    Create use-store-modal.tsx

---
+++ ecommerce-admin/hooks/use-store-modal.tsx
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+interface useStoreModalInterface {
+
+}
+
+const useBearStore = create((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}))
