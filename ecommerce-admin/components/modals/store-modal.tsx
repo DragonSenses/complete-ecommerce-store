84a595a03cbc80ff021d1c5cf9fcc199ba1bbfa3
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-14 14:49:31 -0700

    Update store-modal.tsx

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React from 'react';
-
-import { Modal } from "../ui/modal";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 export default function StoreModal() {
+  const storeModal = useStoreModal();
+
   return (
     <Modal
       title="Create store"
       description="Add a new store to manage products and categories"
-      isOpen={false}
-      onClose={() => {}}
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
     >
       Create Store Modal
     </Modal>
