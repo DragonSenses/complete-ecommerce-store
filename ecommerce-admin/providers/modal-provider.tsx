e123e7e8732827bb3198b5eee5b3d258f448f72e
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-14 15:10:01 -0700

    Update modal-provider.tsx

--- ecommerce-admin/providers/modal-provider.tsx
+++ ecommerce-admin/providers/modal-provider.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
-  
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if(!isMounted) {
+    return null;
+  }
 }
