6242e9c1491614f40dba65ccda70e5e2623232f1
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-15 17:40:52 -0700

    Synchronize Modal with state

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,11 +1,19 @@
 "use client";
+// Global Imports
+import { useEffect } from "react";
 
-import { Modal } from "@/components/ui/modal";
+// Local Imports
 import { useStoreModal } from "@/hooks/use-store-modal";
 
 export default function SetupPage() {
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
 
   return (
     <div className='p-4'>
