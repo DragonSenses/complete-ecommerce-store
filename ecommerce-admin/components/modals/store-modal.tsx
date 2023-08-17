55a323e479387c27943399453b7a03ec0a834cf4
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-16 22:18:02 -0700

    Create a form schema

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+// Global Imports
 import React from 'react';
+import * as z from "zod";
+
+// Local Imports
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from '@/hooks/use-store-modal';
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export default function StoreModal() {
   const storeModal = useStoreModal();
