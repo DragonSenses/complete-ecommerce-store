29b98d5de328ad02775a52b6fe4d58e1cc309752
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-19 18:04:21 -0700

    Update prismadb.ts

--- ecommerce-admin/lib/prismadb.ts
+++ ecommerce-admin/lib/prismadb.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
+// Add prisma to globalThis (to prevent any type warning on variable prisma)
 declare global {
   var prisma: PrismaClient | undefined
 };
 
+// Set prismadb to globalThis prisma instance or create a new instance of PrismaClient
 const prismadb = globalThis.prisma || new PrismaClient();
+
+// Check if environment is in production, then set globalThis
+if(process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
