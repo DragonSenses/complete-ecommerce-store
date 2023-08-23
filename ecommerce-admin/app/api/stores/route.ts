8a4ab6c601d7c6e0a1cc07ce88908433d8414571
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-22 22:05:58 -0700

    Check name field

--- ecommerce-admin/app/api/stores/route.ts
+++ ecommerce-admin/app/api/stores/route.ts
@@ -9,11 +9,22 @@ export async function POST(
     const { userId } = auth();
     // Now have access to currently logged-in userId who is trying to create a new store using our API
 
+    // Extract the body
+    const body = await req.json();
+
+    // Destructure name out of body
+    const { name } = body;
+
     // Send back 401 Unauthorized if userId does not exist
     if(!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
     
+    // Check name field
+    if(!name){
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
   } catch (error){
     console.log('[STORES_POST]', error);
     return new NextResponse("Internal error", { status: 500 });
