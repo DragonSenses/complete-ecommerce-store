f7f75dcc3872090110d759fc7f666d68eb5563e3
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-24 23:37:49 -0700

    Update layout.tsx

--- ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+// Global Imports
+import { auth } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
+import React from 'react';
 
-export default function layout() {
+export default async function DashboardLayout({
+  children,
+  params
+} : {
+  children: React.ReactNode;
+  params: { storeId: string }
+}) {
+  // Check if user is logged-in
+  // Authenticate userId with Clerk
+  const { userId } = auth();
+
+  // If userId does not exist, redirect to sign-in
+  if(!userId) {
+    redirect('/sign-in');
+  }
   return (
     <div>layout</div>
   )
