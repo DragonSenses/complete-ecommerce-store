d582d8eaea5405ed84e6852cdabfb2f19c274577
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-24 20:25:32 -0700

    Add ToastProvider

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -4,8 +4,9 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 // Local Imports
-import { ModalProvider } from '@/providers/modal-provider'
 import './globals.css'
+import { ModalProvider } from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
