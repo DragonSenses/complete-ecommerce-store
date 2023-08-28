59e4f9b169d6f27946def08af14a43ba8be79067
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-27 23:43:42 -0700

    Update Navbar.tsx

--- ecommerce-admin/components/Navbar.tsx
+++ ecommerce-admin/components/Navbar.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav>navbar</nav>
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div className="store-switcher">
+          This will be a <b>Store Switcher</b>
+        </div>
+        <div className="routes">
+          This will be the <b>routes</b>
+        </div>
+      </div>
+    </div>
   )
 }
