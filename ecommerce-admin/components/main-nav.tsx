49b43ce2ad9ce488230cdd4c463536e982dc2c8e
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-28 19:19:21 -0700

    Merge classnames

--- ecommerce-admin/components/main-nav.tsx
+++ ecommerce-admin/components/main-nav.tsx
@@ -1,12 +1,20 @@
 "use client"
 
+// Global Imports
 import React from 'react';
+
+// Local Imports
+import { cn } from '@/lib/utils';
 
 export default function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
   return (
-    <div>Main Navigation</div>
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+    >
+
+    </nav>
   )
 }
