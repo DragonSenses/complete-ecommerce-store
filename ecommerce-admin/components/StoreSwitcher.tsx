39b2c4f1e2ae1c28284bace7133c2b59df372c1a
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-30 01:49:00 -0700

    Create constants needed for return element

--- ecommerce-admin/components/StoreSwitcher.tsx
+++ ecommerce-admin/components/StoreSwitcher.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+// Global Imports
 import React from 'react';
 import { Store } from '@prisma/client';
+import { useParams, useRouter } from 'next/navigation';
 
+// Local Imports
 import { PopoverTrigger } from '@/components/ui/popover';
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -16,6 +20,18 @@ export default function StoreSwitcher({
   // Default value of empty array to safely iterate over even if items are not loaded
   items = [] 
 }: StoreSwitcherProps) {
+  
+  const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id
+  }));
+
+  const currentStore = formattedItems.find((item => item.value === params.storeId));
+
   return (
     <div>StoreSwitcher</div>
   )
