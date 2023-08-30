c3a56ac689d4aa32f30d84c3340836166711ca13
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-30 00:54:27 -0700

    Add StoreSwitcher component

--- ecommerce-admin/components/Navbar.tsx
+++ ecommerce-admin/components/Navbar.tsx
@@ -4,14 +4,13 @@ import React from 'react'
 
 // Local Imports
 import MainNav from '@/components/main-nav'
+import StoreSwitcher from '@/components/StoreSwitcher'
 
 export default function Navbar() {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div className="store-switcher">
-          This will be a <b>Store Switcher</b>
-        </div>
+        <StoreSwitcher />
         <MainNav className="mx-6"/>
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
