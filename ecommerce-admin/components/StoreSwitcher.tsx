e4b09e1c5a6f4dbc25d4543e4b6e008807a2523d
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-30 01:01:18 -0700

    Add PopoverTriggerProps

--- ecommerce-admin/components/StoreSwitcher.tsx
+++ ecommerce-admin/components/StoreSwitcher.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import { PopoverTrigger } from '@/components/ui/popover';
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+}
+
 export default function StoreSwitcher() {
   return (
     <div>StoreSwitcher</div>
