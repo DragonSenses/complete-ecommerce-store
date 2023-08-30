9484a9331a40097a270b89bbca6711bd5119b92c
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-30 01:08:53 -0700

    Update StoreSwitcher.tsx

--- ecommerce-admin/components/StoreSwitcher.tsx
+++ ecommerce-admin/components/StoreSwitcher.tsx
@@ -1,13 +1,21 @@
+"use client";
+
 import React from 'react';
+import { Store } from '@prisma/client';
 
 import { PopoverTrigger } from '@/components/ui/popover';
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
 interface StoreSwitcherProps extends PopoverTriggerProps {
-}
+  items: Store[];
+};
 
-export default function StoreSwitcher() {
+export default function StoreSwitcher({
+  className,
+  // Default value of empty array to safely iterate over even if items are not loaded
+  items = [] 
+}: StoreSwitcherProps) {
   return (
     <div>StoreSwitcher</div>
   )
