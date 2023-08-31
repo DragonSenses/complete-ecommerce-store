7017044b051e56dbf1f19f75959af2bf561b8c80
DragonSenses <kendrickfrancisco@gmail.com>
2023-08-31 02:02:58 -0700

    Implement PopoverTrigger

--- ecommerce-admin/components/StoreSwitcher.tsx
+++ ecommerce-admin/components/StoreSwitcher.tsx
@@ -3,13 +3,14 @@
 // Global Imports
 import React, { useState } from 'react';
 import { Store } from '@prisma/client';
-import { Store as StoreIcon} from 'lucide-react';
+import { ChevronsUpDown, Store as StoreIcon} from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 
 // Local Imports
 import { Button } from '@/components/ui/button';
 import { Popover, PopoverTrigger } from '@/components/ui/popover';
 import { useStoreModal } from '@/hooks/use-store-modal';
+import { cn } from '@/lib/utils';
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -47,8 +48,17 @@ export default function StoreSwitcher({
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <Button>
-          <StoreIcon />
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-expanded={open}
+          aria-label="Select a store"
+          className={cn("w-[200px] justify-between", className)}
+        >
+          <StoreIcon className="mr-2 h-4 w-4" />
+          Current Store
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
     </Popover>
