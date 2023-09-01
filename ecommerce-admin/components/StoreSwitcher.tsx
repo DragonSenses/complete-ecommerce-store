9df0ed7e08731621b5ddb92730f457c0b5a514f6
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-01 00:42:01 -0700

    Dynamically render active store name

--- ecommerce-admin/components/StoreSwitcher.tsx
+++ ecommerce-admin/components/StoreSwitcher.tsx
@@ -58,7 +58,7 @@ export default function StoreSwitcher({
           className={cn("w-[200px] justify-between", className)}
         >
           <StoreIcon className="mr-2 h-4 w-4" />
-          Current Store
+          {currentStore?.label}
           <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
