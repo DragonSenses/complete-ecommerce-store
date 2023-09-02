a089aa589f20db020b21591e45929583d847edeb
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-02 02:51:00 -0700

    Authenticate user and fetch store

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,4 +1,10 @@
+// Global Imports
 import React from 'react';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+// Local Imports
+import prismadb from '@/lib/prismadb';
 
 interface SettingsPageProps {
   params: {
@@ -6,9 +12,31 @@ interface SettingsPageProps {
   }
 };
 
-const SettingsPage: React.FC<SettingsPageProps> = ({
+const SettingsPage: React.FC<SettingsPageProps> = async ({
   params
 }) => {
+
+  // Authenticate userId with Clerk to check if user is logged-in
+  const { userId } = auth();
+  
+  // If userId does not exist, redirect to sign-in page
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  // Find the user's store from the parameters
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId
+    }
+  })
+
+  // If there is no store, then redirect to dashboard
+  if (!store) {
+    redirect("/");
+  }
+
   return (
     <div>
       Settings Page
