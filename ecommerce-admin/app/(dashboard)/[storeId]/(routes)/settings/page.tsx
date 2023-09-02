99ee85ce2bc78b32115551cb11047f1b91ffd026
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-02 03:24:50 -0700

    Pass initial data prop to SettingsForm

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -42,7 +42,7 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <SettingsForm />
+        <SettingsForm initialData={store}/>
       </div>
     </div>
   )
