f1baec140e48f6f8e4cc99c6f0fb3571bd61bd96
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-03 03:29:55 -0700

    Update heading.tsx

--- ecommerce-admin/components/ui/heading.tsx
+++ ecommerce-admin/components/ui/heading.tsx
@@ -11,8 +11,8 @@ export const Heading: React.FC<HeadingProps> = ({
 }) => {
   return (
     <div>
-      <h2>{title}</h2>
-      <p>
+      <h2 className='text-3xl font-bold tracking-tight'>{title}</h2>
+      <p className='text-sm text-muted-foreground'>
         {description}
       </p>
     </div>
