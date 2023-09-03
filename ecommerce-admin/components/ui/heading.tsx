1775dbdd5b599d98babdb4a6c8982a1756c4b053
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-03 03:03:00 -0700

    Create heading.tsx

---
+++ ecommerce-admin/components/ui/heading.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface HeadingProps {
+  title: string;
+  description: string;
+};
+
+export const Heading: React.FC<HeadingProps> = ({
+  title,
+  description
+}) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>
+        {description}
+      </p>
+    </div>
+  )
+};
