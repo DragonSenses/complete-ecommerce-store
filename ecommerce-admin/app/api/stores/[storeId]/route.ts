31bb87e1220b59817343a4c907d10c41ae2feb60
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-06 04:30:45 -0700

    Update PATCH route

--- ecommerce-admin/app/api/stores/[storeId]/route.ts
+++ ecommerce-admin/app/api/stores/[storeId]/route.ts
@@ -1,6 +1,22 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
+
 export async function PATCH (
   req: Request,
   { params }: { params: { storeId: string }}
 ){
-  
+  try {
+    // Authenticate userId with Clerk to check if user is logged-in
+    const { userId } = auth();
+    
+    // If userId does not exist, redirect to sign-in page
+    if (!userId) {
+      redirect("/sign-in");
+  }
+
+  } catch (error) {
+    console.log('[STORE_PATCH]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
 }
