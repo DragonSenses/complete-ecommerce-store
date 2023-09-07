7941b71d33d8350b6b0f5e1e36990c4363c63967
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-07 04:39:39 -0700

    Create AlertModal.tsx

---
+++ ecommerce-admin/components/modals/AlertModal.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+}
+
+export const AlertModal: React.FC<AlertModalProps> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  loading
+}) => {
+  return (
+    <div>AlertModal</div>
+  )
+}
