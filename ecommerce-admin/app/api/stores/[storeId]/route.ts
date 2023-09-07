813c5c30134ac6976964b3b2c3af5b2574511074
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-07 03:55:33 -0700

    Implement delete function

--- ecommerce-admin/app/api/stores/[storeId]/route.ts
+++ ecommerce-admin/app/api/stores/[storeId]/route.ts
@@ -17,7 +17,8 @@ export async function PATCH (
 
     // Extract body from the request
     const body = await req.json();
-
+    
+    // Destructure the body
     const { name } = body;
 
     if (!name) {
@@ -44,4 +45,36 @@ export async function PATCH (
     console.log('[STORE_PATCH]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
+
+export async function DELETE (
+  req: Request,
+  { params }: { params: { storeId: string }}
+){
+  try {
+    // Authenticate userId with Clerk to check if user is logged-in
+    const { userId } = auth();
+    
+    // If userId does not exist send back 401 response
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.storeId){
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    // Find and Delete store
+    const store = await prismadb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId
+      }
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORE_DELETE]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
