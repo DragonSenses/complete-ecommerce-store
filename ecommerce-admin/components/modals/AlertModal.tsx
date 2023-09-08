33312ace826667789b1bdd2a9eb4d8e72314c55d
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-08 03:26:56 -0700

    Return Modal component
    - Add descriptive comments
    - Add Button components inside the return
    - Pass data to props accordingly

--- ecommerce-admin/components/modals/AlertModal.tsx
+++ ecommerce-admin/components/modals/AlertModal.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 
 // Local Imports
+import { Button } from '@/components/ui/button';
 import { Modal } from '@/components/ui/modal';
 
 interface AlertModalProps {
@@ -17,19 +18,36 @@ export const AlertModal: React.FC<AlertModalProps> = ({
   onConfirm,
   loading
 }) => {
+  // Declare isMounted state variable and initialize it to false
   const [isMounted, setIsMounted] = useState(false);
 
+  // useEffect hook to set isMounted variable to true
+  // Delays the execution of client-side-only code until after hydration
   useEffect(() => {
     setIsMounted(true);
-  }, []);
+  }, []); // Only run once after the initial render
 
+  // Prevent rendering of the component before the effect has run
+  // To protect from hydration errors or unwanted flashes of content
   if (!isMounted) {
     return null;
   }
 
   return (
     <Modal
-
+      title="Are you sure?"
+      description="This action is irreversible!"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+        <Button disabled={loading} variant="outline" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={loading} variant="destructive" onClick={onConfirm}>
+          Continue
+        </Button>
+      </div>
     </Modal>
   )
 }
