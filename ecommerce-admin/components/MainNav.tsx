9c6fd550308e3d9f8baa4a393cd23bff1a5f7919
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-09 05:48:17 -0700

    Add Home route to MainNav
    - Add a way for the user to navigate back to the dashboard

--- ecommerce-admin/components/MainNav.tsx
+++ ecommerce-admin/components/MainNav.tsx
@@ -17,6 +17,11 @@ export default function MainNav({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: 'Overview',
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: 'Settings',
       active: pathname === `/${params.storeId}/settings`,
