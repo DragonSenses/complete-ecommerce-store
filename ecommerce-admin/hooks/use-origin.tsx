9189fd622090e8b088a75360f10b579bce6d44b7
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-11 13:25:38 -0700

    Create use-origin.tsx

---
+++ ecommerce-admin/hooks/use-origin.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+export const useOrigin = () => {
+  const [mounted, setMounted] = useState(false);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
