c7f1ba41a9f5803825a431031026df24c91e18fd
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-13 11:30:14 -0700

    Add billboard route to Nav

--- ecommerce-admin/components/MainNav.tsx
+++ ecommerce-admin/components/MainNav.tsx
@@ -22,6 +22,11 @@ export default function MainNav({
       active: pathname === `/${params.storeId}`,
     },
     {
+      href: `/${params.storeId}/billboards`,
+      label: 'Billboards',
+      active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: 'Settings',
       active: pathname === `/${params.storeId}/settings`,
