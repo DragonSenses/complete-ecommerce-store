abe2b6df0b80753fab2d39082e4df7c61a71743d
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-13 11:41:04 -0700

    Add BillboardClient in output

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,9 +1,17 @@
+// Global Imports
 import React from 'react';
 
-const BillBoardsPage = () => {
+// Local Imports
+import BillboardClient from './components/client';
+
+const BillboardsPage = () => {
   return (
-    <div>BillBoardsPage</div>
+    <div className='flex-col'>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient />
+      </div>
+    </div>
   );
 }
 
-export default BillBoardsPage;
+export default BillboardsPage;
