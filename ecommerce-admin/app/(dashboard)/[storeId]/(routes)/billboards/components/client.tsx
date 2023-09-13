c9b4a7f22cd25002a048c57abd5b1ec6fe5f5d91
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-13 12:05:17 -0700

    Update BillboardClient output

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,12 +1,29 @@
-"use client"
+"use client";
+
+// Global Imports
+import { Plus } from "lucide-react";
+
+// Local Imports
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
 
 // Client component that loads all our Billboards
 const BillboardClient = () => {
   return (
     <>
       <div className="flex items-center justify-between">
-        Billboard Client
+        <Heading
+          title="Billboards (0)"
+          description="Manage billboards for your store"
+        />
+        <Button>
+          <Plus className="h-4 w-4 mr-2" />
+          Add New
+        </Button>
       </div>
+      <Separator />
+      {/* Data Table */}
     </>
   )
 }
