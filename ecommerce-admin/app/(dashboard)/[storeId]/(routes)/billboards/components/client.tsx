ce3244fe638c61615d09a031112c1ac0dc35901c
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-13 11:40:50 -0700

    Create BillboardClient component

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+// Client component that loads all our Billboards
+const BillboardClient = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        Billboard Client
+      </div>
+    </>
+  )
+}
+
+export default BillboardClient;
