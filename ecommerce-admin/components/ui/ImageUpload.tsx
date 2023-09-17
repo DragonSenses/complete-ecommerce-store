4b24e2f24f877f278127d28ca9ee1df930b21806
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-16 22:00:01 -0700

    Update ImageUpload output

--- ecommerce-admin/components/ui/ImageUpload.tsx
+++ ecommerce-admin/components/ui/ImageUpload.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Trash } from 'lucide-react';
 
 interface ImageUploadProps {
   disabled?: boolean;
@@ -36,7 +38,24 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   }
 
   return (
-    <div>ImageUpload</div>
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
+            <div className="z-10 aboslute top-2 right-2">
+              <Button
+               type="button"
+               onClick={() => onRemove(url)} 
+               variant="destructive"
+               size="icon"
+              > 
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
