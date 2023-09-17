08e94098a23fdc33244625ef7da72b67a9ca4f89
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-16 21:37:14 -0700

    Add props to ImageUpload

--- ecommerce-admin/components/ui/ImageUpload.tsx
+++ ecommerce-admin/components/ui/ImageUpload.tsx
@@ -2,7 +2,22 @@
 
 import React from 'react';
 
-const ImageUpload = () => {
+interface ImageUploadProps {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value
+}) => {
+
+  
+
   return (
     <div>ImageUpload</div>
   )
