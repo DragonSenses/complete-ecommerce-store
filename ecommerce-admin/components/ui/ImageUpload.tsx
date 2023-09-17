20031e253d874ed083386f937693291240ca6a73
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-16 21:39:08 -0700

    Add side-effect to ImageUpload
    - Prevent rendering of component before it is mounted
    - Protect against hydration errors & unwanted flashes of content

--- ecommerce-admin/components/ui/ImageUpload.tsx
+++ ecommerce-admin/components/ui/ImageUpload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface ImageUploadProps {
   disabled?: boolean;
@@ -15,8 +15,20 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   onRemove,
   value
 }) => {
+  // Declare isMounted state variable and initialize it to false
+  const [isMounted, setIsMounted] = useState(false);
 
-  
+  // useEffect hook to set isMounted variable to true
+  // Delays the execution of client-side-only code until after hydration
+  useEffect(() => {
+    setIsMounted(true);
+  }, []); // Only run once after the initial render
+
+  // Prevent rendering of the component before the effect has run
+  // To protect from hydration errors or unwanted flashes of content
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <div>ImageUpload</div>
