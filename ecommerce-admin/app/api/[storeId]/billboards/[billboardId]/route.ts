8137c591e5c5d6b581e26b3e41203d0ff4c2451f
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-19 14:50:34 -0700

    Create routes for specific Billboard

---
+++ ecommerce-admin/app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,80 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH (
+  req: Request,
+  { params }: { params: { storeId: string }}
+){
+  try {
+    // Authenticate userId with Clerk to check if user is logged-in
+    const { userId } = auth();
+    
+    // If userId does not exist send back 401 response
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    // Extract body from the request
+    const body = await req.json();
+    
+    // Destructure the body
+    const { name } = body;
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!params.storeId){
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    // Find and Update store
+    const store = await prismadb.store.updateMany({
+      where: {
+        id: params.storeId,
+        userId
+      },
+      data: {
+        name
+      }
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORE_PATCH]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
+
+export async function DELETE (
+  req: Request,
+  { params }: { params: { storeId: string }}
+){
+  try {
+    // Authenticate userId with Clerk to check if user is logged-in
+    const { userId } = auth();
+    
+    // If userId does not exist send back 401 response
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.storeId){
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    // Find and Delete store
+    const store = await prismadb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId
+      }
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORE_DELETE]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
