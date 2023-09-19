edd28f5267711286c1f0a7c5eacb2450cd4bd8e0
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-19 15:41:54 -0700

    Update Delete route

--- ecommerce-admin/app/api/[storeId]/billboards/[billboardId]/route.ts
+++ ecommerce-admin/app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -73,9 +73,18 @@ export async function PATCH (
 
 export async function DELETE (
   req: Request,
-  { params }: { params: { storeId: string }}
+  { params }: { params: { storeId: string, billboardId: string }}
 ){
   try {
+    // Check parameters
+    if (!params.storeId){
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+
+    if (!params.billboardId){
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
     // Authenticate userId with Clerk to check if user is logged-in
     const { userId } = auth();
     
@@ -84,21 +93,30 @@ export async function DELETE (
       return new NextResponse("Unauthenticated", { status: 401 });
     }
 
-    if (!params.storeId){
-      return new NextResponse("Store id is required", { status: 400 });
-    }
-
-    // Find and Delete store
-    const store = await prismadb.store.deleteMany({
+    // Check database if store exists for current user
+    const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
         userId
       }
     });
 
-    return NextResponse.json(store);
+    // User is logged-in but does not have permission to modify the store
+    if (!storeByUserId) {
+      // Respond with 403 Forbidden, current user is unauthorized to modify
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    // Find and Delete Billboard
+    const billboard = await prismadb.billboard.deleteMany({
+      where: {
+        id: params.billboardId
+      }
+    });
+
+    return NextResponse.json(billboard);
   } catch (error) {
-    console.log('[STORE_DELETE]', error);
+    console.log('[BILLBOARD_DELETE]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 };
