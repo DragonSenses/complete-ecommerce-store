9ea50285522f3204e13422a305e92a6f4427cb78
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-19 14:46:40 -0700

    Update GET route

--- ecommerce-admin/app/api/[storeId]/billboards/route.ts
+++ ecommerce-admin/app/api/[storeId]/billboards/route.ts
@@ -60,7 +60,7 @@ export async function POST(
       }
     });
 
-    // Send back response with the store
+    // Send back response with the billboard
     return NextResponse.json(billboard);
   } catch (error){
     console.log('[BILLBOARDS_POST]', error);
@@ -73,62 +73,22 @@ export async function GET(
   { params }: { params: { storeId: string } }
 ) {
   try {
-    // Use Clerk to authenticate POST route
-    const { userId } = auth();
-
-    // Send back 401, unauthenticated if user is not logged-in
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 });
-    }
-
-    // Extract the body
-    const body = await req.json();
-
-    // Destructure fields out of body
-    const { label, imageUrl } = body;
-
-    // Check label field
-    if (!label){
-      return new NextResponse("Label is required", { status: 400 });
-    }
-
-    // Check imageUrl field
-    if (!imageUrl){
-      return new NextResponse("Image URL is required", { status: 400 });
-    }
-
     // Check if storeId exists
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
     }
 
-    // Check database if store exists for current user
-    const storeByUserId = await prismadb.store.findFirst({
+    // Find all billboards available in that store in the database
+    const billboards = await prismadb.billboard.findMany({
       where: {
-        id: params.storeId,
-        userId
-      }
-    });
-
-    // User is logged-in but does not have permission to modify the store
-    if (!storeByUserId) {
-      // Respond with 403 Forbidden, current user is unauthorized to modify
-      return new NextResponse("Unauthorized", { status: 403 });
-    }
-
-    // Create billboard for user's specific store in the database
-    const billboard = await prismadb.billboard.create({
-      data: {
-        label,
-        imageUrl,
         storeId: params.storeId
       }
     });
 
-    // Send back response with the store
-    return NextResponse.json(billboard);
+    // Send back response with all billboards
+    return NextResponse.json(billboards);
   } catch (error){
-    console.log('[BILLBOARDS_POST]', error);
+    console.log('[BILLBOARDS_GET]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
