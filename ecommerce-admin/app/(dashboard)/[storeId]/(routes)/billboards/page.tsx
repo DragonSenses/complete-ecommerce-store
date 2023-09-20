37da65018455c0c3148ed76f6885062814e89d53
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-20 11:43:46 -0700

    Update BillboardPage
    - Fetch all Billboards of the active store and sort by newest

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,26 @@
 // Global Imports
 import React from 'react';
+import prismadb from '@/lib/prismadb';
 
 // Local Imports
 import BillboardClient from './components/client';
 
-const BillboardsPage = () => {
+const BillboardsPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  
+  // Fetch all Billboards specific to the active store
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
   return (
     <div className='flex-col'>
       <div className="flex-1 space-y-4 p-8 pt-6">
