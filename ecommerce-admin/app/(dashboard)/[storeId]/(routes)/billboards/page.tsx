86af53a0f45a829441c9f89b400de201493c7eb3
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-20 13:19:28 -0700

    Pass data into client
    After the fetch, pass all billboards as data to BillboardClient

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -24,7 +24,7 @@ const BillboardsPage = async ({
   return (
     <div className='flex-col'>
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={billboards}/>
       </div>
     </div>
   );
