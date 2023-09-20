a1558e109de5430ad2d4e47c7e9b6621abc19fc9
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-20 11:41:52 -0700

    Update ImageUpload.tsx
    - Fix typo on layout/position utility on "absolute" which disrupted the output of the Button
    - Added `sizes` prop with multiple options to improve Image performance
    
    https://nextjs.org/docs/pages/api-reference/components/image#sizes

--- ecommerce-admin/components/ui/ImageUpload.tsx
+++ ecommerce-admin/components/ui/ImageUpload.tsx
@@ -47,13 +47,13 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
       <div className="mb-4 flex items-center gap-4">
         {value.map((url) => (
           <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
-            <div className="z-10 aboslute top-2 right-2">
+            <div className="z-10 absolute top-2 right-2">
               <Button
                type="button"
                onClick={() => onRemove(url)} 
                variant="destructive"
                size="icon"
-              > 
+              >
                 <Trash className="h-4 w-4" />
               </Button>
             </div>
@@ -62,6 +62,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               className="object-cover"
               alt="Image"
               src={url}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
         ))}
