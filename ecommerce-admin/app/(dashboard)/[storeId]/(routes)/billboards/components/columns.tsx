8a16b962d375ce876bb1956b9483f801f0fc8ccc
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-23 13:27:12 -0700

    Pass original row data into CellAction

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -22,6 +22,6 @@ export const columns: ColumnDef<BillboardColumn>[] = [
   },
   {
     id: "actions",
-    cell: () => <CellAction />
+    cell: ({ row }) => <CellAction data={row.original} />
   },
 ]
