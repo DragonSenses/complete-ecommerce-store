1b56b42ab8251eec63feb315bf97253cebee4e1a
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-23 13:49:54 -0700

    Update cell-action output
    Add the following to output:
    - DropdownMenu
    - DropdownMenuTrigger
    - Button
    - icon

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,7 +1,20 @@
 "use client";
 
+// Global Imports
 import React from 'react';
+import { MoreHorizontal } from "lucide-react"
+ 
+// Local Imports
 import { BillboardColumn } from './columns';
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 interface CellActionProps {
   data: BillboardColumn
@@ -11,6 +24,13 @@ export const CellAction: React.FC<CellActionProps> = ({
   data
 }) => {
   return (
-    <div>cell-action</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
   )
 }
