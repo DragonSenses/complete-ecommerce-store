864aa1e13e9d7392b6564093249ceb205815a6b7
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-22 21:43:59 -0700

    Add pagination functionality

--- ecommerce-admin/components/ui/data-table.tsx
+++ ecommerce-admin/components/ui/data-table.tsx
@@ -4,6 +4,7 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 
@@ -15,6 +16,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+
+
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -29,6 +32,7 @@ export function DataTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   })
 
   return (
