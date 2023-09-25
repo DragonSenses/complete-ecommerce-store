76757a4d37c18e74205bc7c2a3572a74efdd2d8a
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-25 14:15:35 -0700

    Create delete event handler

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 // Global Imports
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react"
 import { useParams, useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
@@ -25,14 +26,44 @@ interface CellActionProps {
 export const CellAction: React.FC<CellActionProps> = ({
   data
 }) => {
+  // Create router object to perform client-side navigation (pushing to new URL)
   const router = useRouter();
+  
+  // Hook returns an object containing current route's filled in dynamic parameters
   const params = useParams();
+
+  // Create open state to control the Alert modal
+  const [open, setOpen] = useState(false);
+
+  // Create loading state to disable interactive elements
+  const [loading, setLoading] = useState(false);
 
   // Copy Event Handler
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
     toast.success("Billboard ID copied to clipboard.");
   }
+
+  // Define a delete handler
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      // Call an API with dynamic route to delete the store
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+      // Re-synchronize server component to update data
+      router.refresh();
+      // Push user back to root layout where we check if there is another existing store
+      router.push("/");
+      toast.success("Billboard deleted.");
+    } catch (error) {
+      // Safety mechanism will prompt a warning to delete any related records to the Billboard
+      toast.error("Make sure you removed all categories using this billboard first.");
+    } finally {
+      setLoading(false);
+      // Close the Modal
+      setOpen(false);
+    }
+  };
 
   return (
     <DropdownMenu>
