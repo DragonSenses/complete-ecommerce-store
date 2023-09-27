fe9845c8f858c5e73b46ed3d7426ae20d6c9a0a0
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-26 20:34:59 -0700

    Update ApiList.tsx

--- ecommerce-admin/components/ui/ApiList.tsx
+++ ecommerce-admin/components/ui/ApiList.tsx
@@ -1,9 +1,35 @@
 "use client";
 
+// Global imports
 import React from 'react';
+import { useParams } from 'next/navigation';
 
-export const ApiList = () => {
+// Local imports
+import { useOrigin } from '@/hooks/use-origin';
+import { ApiAlert } from '@/components/ui/ApiAlert';
+
+
+interface ApiListProps {
+  entityName: string;
+  entityIdName: string;
+}
+
+export const ApiList: React.FC<ApiListProps> = ({
+  entityName,
+  entityIdName,
+}) => {
+  // Safely access the window object, only after the component is mounted
+  const origin = useOrigin();
+
+  // Hook returns an object containing current route's filled in dynamic parameters
+  const params = useParams();
+
+  // Create base URL for the API call
+  const baseUrl = `${origin}/api/${params.storeId}`;
+
   return (
-    <div>ApiList</div>
+    <>
+
+    </>
   )
 }
