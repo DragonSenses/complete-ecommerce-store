70e3cd0f1b110a13beec6a2f5c164a37e3e9121d
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-28 17:28:04 -0700

    Create page.tsx

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,43 @@
+// Global Imports
+import React from 'react';
+import prismadb from '@/lib/prismadb';
+import { format } from 'date-fns';
+
+// Local Imports
+import CategoryClient from './components/client';
+import { CategoryColumn } from './components/columns';
+
+const CategoriesPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  
+  // Fetch all Categories specific to the active store
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  // Format each Category into a CategoryColumn
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }))
+
+  return (
+    <div className='flex-col'>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryClient data={formattedCategories}/>
+      </div>
+    </div>
+  );
+}
+
+export default CategoriesPage;
