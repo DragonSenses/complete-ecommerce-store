6b19432c2f9f9c48cfa50a4c1447222fd52ca8c5
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-29 22:10:08 -0700

    Update CategoryForm.tsx

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/CategoryForm.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/CategoryForm.tsx
@@ -4,7 +4,7 @@
 import * as z from 'zod';
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Billboard } from '@prisma/client';
+import { Category } from '@prisma/client';
 import { toast } from 'react-hot-toast';
 import { Trash } from 'lucide-react';
 import { useForm } from 'react-hook-form';
@@ -34,14 +34,14 @@ const formSchema = z.object({
 });
 
 // extract the inferred type
-type BillboardFormValues = z.infer<typeof formSchema>;
+type CategoryFormValues = z.infer<typeof formSchema>;
 
 // Define type and shape of props
-interface BillboardFormProps {
-  initialData: Billboard | null
+interface CategoryFormProps {
+  initialData: Category | null
 }
 
-const BillboardForm: React.FC<BillboardFormProps> = ({
+const CategoryForm: React.FC<CategoryFormProps> = ({
   initialData
 }) => {
   // Create router object to perform client-side navigation
@@ -57,13 +57,13 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
   const [loading, setLoading] = useState(false);
 
   // Create dynamic data to pass into output
-  const title = initialData ? "Edit billboard" : "Create billboard";
-  const description = initialData ? "Edit a billboard" : "Add a new billboard";
-  const toastMessage = initialData ? "Billboard updated." : "Billboard created.";
+  const title = initialData ? "Edit category" : "Create category";
+  const description = initialData ? "Edit a category" : "Add a new category";
+  const toastMessage = initialData ? "Category updated." : "Category created.";
   const action = initialData ? "Save changes" : "Create";
 
   // 1. Define form with useForm hook & zodResolver for validation
-  const form = useForm<BillboardFormValues>({
+  const form = useForm<CategoryFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
       label: '',
@@ -72,22 +72,22 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
   });
 
   // 2. Define a submit handler
-  const onSubmit = async (data: BillboardFormValues) => {
+  const onSubmit = async (data: CategoryFormValues) => {
     try {
       setLoading(true);
       if (initialData) {
-        // Update specific Billboard
-        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+        // Update a specific Category
+        await axios.patch(`/api/${params.storeId}/categories/${params.categoryId}`, data);
       } else {
-        // Create new Billboard
-        await axios.post(`/api/${params.storeId}/billboards`, data);
+        // Create a new Category
+        await axios.post(`/api/${params.storeId}/categories`, data);
       }
       // Refresh current route to make new request to server
       // Re-fetch data requests & re-render server components
       // Re-initializes initialData
       router.refresh();
-      // Re-route the user to the Billboards page
-      router.push(`/${params.storeId}/billboards`)
+      // Re-route the user to the Categorys page
+      router.push(`/${params.storeId}/categories`)
       // Success notification with dynamic message
       toast.success(toastMessage);
     } catch (error) {
@@ -101,16 +101,16 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
   const onDelete = async () => {
     try {
       setLoading(true);
-      // Call an API with dynamic route to delete the Billboard
-      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+      // Call an API with dynamic route to delete the Category
+      await axios.delete(`/api/${params.storeId}/categories/${params.categoryId}`);
       // Re-synchronize server component to update data
       router.refresh();
-      // Navigate back to the specific store's billboards page after deletion
-      router.push(`${params.storeId}/billboards`);
-      toast.success("Billboard deleted.");
+      // Navigate back to the specific store's categories page after deletion
+      router.push(`${params.storeId}/categories`);
+      toast.success("Category deleted.");
     } catch (error) {
-      // Safety mechanism will prompt a warning to delete any related records to the Billboard
-      toast.error("Make sure you removed all categories using this billboard first.");
+      // Safety mechanism will prompt a warning to delete any related records to the Category
+      toast.error("Make sure you removed all categories using this category first.");
     } finally {
       setLoading(false);
       // Close the Modal
@@ -153,7 +153,7 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
                 <FormItem>
                   <FormLabel>Label</FormLabel>
                   <FormControl>
-                    <Input disabled={loading} placeholder="Billboard label" {...field} />
+                    <Input disabled={loading} placeholder="Category label" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -187,4 +187,4 @@ const BillboardForm: React.FC<BillboardFormProps> = ({
   )
 }
 
-export default BillboardForm
+export default CategoryForm
