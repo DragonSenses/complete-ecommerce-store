7316be014f8adcf627b77d7b62553acdcb5cf387
DragonSenses <kendrickfrancisco@gmail.com>
2023-09-29 21:29:15 -0700

    Update CategoryColumn

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -5,16 +5,22 @@ import { ColumnDef } from "@tanstack/react-table"
 import { CellAction } from "./cell-action"
 
 // This type is used to define the shape of our data.
-export type BillboardColumn = {
+export type CategoryColumn = {
   id: string
-  label: string
+  name: string
+  billboardLabel: string
   createdAt: string
 }
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<CategoryColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell: ({ row }) => row.original.billboardLabel,
   },
   {
     accessorKey: "createdAt",
