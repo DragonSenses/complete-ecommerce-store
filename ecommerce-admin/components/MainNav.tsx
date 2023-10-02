30e17fe7b6e30230e055281a3af36a29d7d5b8fa
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-02 02:59:14 -0700

    Add sizes to main navigation routes

--- ecommerce-admin/components/MainNav.tsx
+++ ecommerce-admin/components/MainNav.tsx
@@ -32,6 +32,11 @@ export default function MainNav({
       active: pathname === `/${params.storeId}/categories`,
     },
     {
+      href: `/${params.storeId}/sizes`,
+      label: 'Sizes',
+      active: pathname === `/${params.storeId}/sizes`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: 'Settings',
       active: pathname === `/${params.storeId}/settings`,
