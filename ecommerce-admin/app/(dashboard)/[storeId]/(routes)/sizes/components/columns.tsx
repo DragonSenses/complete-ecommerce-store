113068384db15a50a1b680b16a606b22b9346548
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-03 01:50:48 -0700

    Update SizeColumn

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -5,16 +5,21 @@ import { ColumnDef } from "@tanstack/react-table"
 import { CellAction } from "./cell-action"
 
 // This type is used to define the shape of our data.
-export type BillboardColumn = {
+export type SizeColumn = {
   id: string
-  label: string
+  name: string
+  value: string
   createdAt: string
 }
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<SizeColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "value",
+    header: "Value",
   },
   {
     accessorKey: "createdAt",
