4a0dcc1e90f68fd5b96f4d3bdc315dfa2a2f4e5d
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-03 01:21:33 -0700

    Create columns.tsx

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { CellAction } from "./cell-action"
+
+// This type is used to define the shape of our data.
+export type BillboardColumn = {
+  id: string
+  label: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<BillboardColumn>[] = [
+  {
+    accessorKey: "label",
+    header: "Label",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  },
+]
