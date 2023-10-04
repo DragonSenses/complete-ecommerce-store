6078b6ae42e779e6fac6f6d2a7321fa5003ecee5
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-04 02:41:48 -0700

    Update Sizes Client

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -5,19 +5,19 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
 // Local Imports
-import { BillboardColumn, columns } from "./columns";
+import { SizeColumn, columns } from "./columns";
 import { ApiList } from "@/components/ui/ApiList";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
-interface BillboardClientProps {
-  data: BillboardColumn[]
+interface SizesClientProps {
+  data: SizeColumn[]
 }
 
-// Client component that loads all our Billboards
-const BillboardClient: React.FC<BillboardClientProps> = ({
+// Client component that loads all our Sizes
+const SizesClient: React.FC<SizesClientProps> = ({
   data
 }) => {
   // Create router object to perform client-side navigation
@@ -30,26 +30,26 @@ const BillboardClient: React.FC<BillboardClientProps> = ({
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
+          title={`Sizes (${data.length})`}
+          description="Manage sizes for your store"
         />
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+          onClick={() => router.push(`/${params.storeId}/sizes/new`)}
         >
           <Plus className="h-4 w-4 mr-2" />
           Add New
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey={"label"}/>
-      <Heading title="API" description="API calls for Billboards" />
+      <DataTable columns={columns} data={data} searchKey={"name"}/>
+      <Heading title="API" description="API calls for Sizes" />
       <Separator />
       <ApiList 
-        entityName="billboards"
-        entityIdName="billboardId"
+        entityName="sizes"
+        entityIdName="sizeId"
       />
     </>
   )
 }
 
-export default BillboardClient;
+export default SizesClient;
