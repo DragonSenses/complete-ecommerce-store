f9dcbe1275867b53d39f3acafa43dde7ae5503a0
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-06 19:00:44 -0700

    Create colors page

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,43 @@
+// Global Imports
+import React from 'react';
+import prismadb from '@/lib/prismadb';
+import { format } from 'date-fns';
+
+// Local Imports
+import ColorsClient from './components/client';
+import { ColorColumn } from './components/columns';
+
+const ColorsPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  
+  // Fetch all colors specific to the active store
+  const colors = await prismadb.color.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  // Format each Color into a ColorColumn
+  const formattedColors: ColorColumn[] = colors.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }))
+
+  return (
+    <div className='flex-col'>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ColorsClient data={formattedColors}/>
+      </div>
+    </div>
+  );
+}
+
+export default ColorsPage;
