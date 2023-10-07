1fcb66c5ec7a1f9eb1b61649df3c69d7a012894b
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-07 04:47:15 -0700

    Create cell-action.tsx

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+// Global Imports
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react"
+import { useParams, useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+
+// Local Imports
+import { AlertModal } from '@/components/modals/AlertModal';
+import { SizeColumn } from './columns';
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+interface CellActionProps {
+  data: SizeColumn
+}
+
+export const CellAction: React.FC<CellActionProps> = ({
+  data
+}) => {
+  // Create router object to perform client-side navigation
+  const router = useRouter();
+
+  // Hook returns an object containing current route's filled in dynamic parameters
+  const params = useParams();
+
+  // Create open state to control the Alert modal
+  const [open, setOpen] = useState(false);
+
+  // Create loading state to disable interactive elements
+  const [loading, setLoading] = useState(false);
+
+  // Copy Event Handler
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Size ID copied to clipboard.");
+  }
+
+  // Define a delete handler
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      // Call an API with dynamic route to delete the Size
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+      // Refresh current route to make new request to server
+      // Re-fetch data requests & re-render server components
+      router.refresh();
+      toast.success("Size deleted.");
+    } catch (error) {
+      // Safety mechanism will prompt a warning to delete any related records
+      toast.error("Make sure you removed all products using this size first.");
+    } finally {
+      setLoading(false);
+      // Close the Modal
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal 
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={(onDelete)}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>
+            Actions
+          </DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className="mr-2 h-4 w-4" />
+            Copy ID
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="mr-2 h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
