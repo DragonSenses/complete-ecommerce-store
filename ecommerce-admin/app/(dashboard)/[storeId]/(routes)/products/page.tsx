8c5a342b6ed50debd6714be2a9b96ca60aafebac
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-10 13:26:26 -0700

    Create product page

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,50 @@
+// Global Imports
+import React from 'react';
+import prismadb from '@/lib/prismadb';
+import { format } from 'date-fns';
+
+// Local Imports
+import ProductClient from './components/client';
+import { ProductColumn } from './components/columns';
+
+const ProductsPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  
+  // Fetch all products specific to the active store
+  const products = await prismadb.product.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    include: {
+      category: true,
+      size: true,
+      color: true,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  // Format each Billboard into a ProductColumn
+  const formattedProducts: ProductColumn[] = products.map((item) => ({
+    id: item.id,
+    name: item.name,
+    isArchived: item.isArchived,
+    isFeatured: item.isFeatured,
+    price: item.price,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }))
+
+  return (
+    <div className='flex-col'>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductClient data={formattedProducts}/>
+      </div>
+    </div>
+  );
+}
+
+export default ProductsPage;
