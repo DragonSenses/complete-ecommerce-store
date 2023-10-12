48754ad86e21235e80c202c55ebd30f274f193d4
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-11 19:01:47 -0700

    Create columns.tsx

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { CellAction } from "./cell-action"
+
+// This type is used to define the shape of our data.
+export type ProductColumn = {
+  id: string;
+  name: string;
+  price: string;
+  size: string;
+  color: string;
+  isArchived: boolean;
+  isFeatured: boolean;
+  createdAt: string;
+}
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.color}
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{ backgroundColor: row.original.color }}
+        />
+      </div>
+    )
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  },
+]
