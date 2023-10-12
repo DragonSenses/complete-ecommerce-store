47896a9503400f240d13aa24c6a9be4ff10b4f31
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-11 18:52:42 -0700

    Add price formatter

--- ecommerce-admin/lib/utils.ts
+++ ecommerce-admin/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const priceFormatter = new Intl.NumberFormat("en-US", {
+  style: 'currency',
+  currency: 'USD'
+});
