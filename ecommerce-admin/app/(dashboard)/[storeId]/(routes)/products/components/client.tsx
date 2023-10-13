099dd92fd46c73019ea424b98ea95b33e3836adf
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-12 23:26:30 -0700

    Update product client

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -5,19 +5,19 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
 // Local Imports
-import { BillboardColumn, columns } from "./columns";
+import { ProductColumn, columns } from "./columns";
 import { ApiList } from "@/components/ui/ApiList";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
-interface BillboardClientProps {
-  data: BillboardColumn[]
+interface ProductClientProps {
+  data: ProductColumn[]
 }
 
-// Client component that loads all our Billboards
-const BillboardClient: React.FC<BillboardClientProps> = ({
+// Client component that loads all our products
+const ProductClient: React.FC<ProductClientProps> = ({
   data
 }) => {
   // Create router object to perform client-side navigation
@@ -30,26 +30,26 @@ const BillboardClient: React.FC<BillboardClientProps> = ({
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
+          title={`Products (${data.length})`}
+          description="Manage products for your store"
         />
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+          onClick={() => router.push(`/${params.storeId}/products/new`)}
         >
           <Plus className="h-4 w-4 mr-2" />
           Add New
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey={"label"}/>
-      <Heading title="API" description="API calls for Billboards" />
+      <DataTable columns={columns} data={data} searchKey={"name"}/>
+      <Heading title="API" description="API calls for Products" />
       <Separator />
       <ApiList 
-        entityName="billboards"
-        entityIdName="billboardId"
+        entityName="products"
+        entityIdName="productId"
       />
     </>
   )
 }
 
-export default BillboardClient;
+export default ProductClient;
