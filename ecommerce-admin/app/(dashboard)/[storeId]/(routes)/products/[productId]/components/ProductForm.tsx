e4690976c62b01d02ccf7eb6c76325b4b95d0297
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-15 13:36:56 -0700

    Update ProductForm.tsx
    - Update the output with form fields for featured and archived property
    - Each form field contains a checkbox

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/ProductForm.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/ProductForm.tsx
@@ -21,6 +21,7 @@ import { Separator } from '@/components/ui/separator';
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -341,6 +342,36 @@ const ProductForm: React.FC<ProductFormProps> = ({
                       onCheckedChange={field.onChange}
                     />
                   </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>
+                      Featured
+                    </FormLabel>
+                    <FormDescription>
+                      This product will appear on the home page
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="isArchived"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl >
+                    <Checkbox 
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>
+                      Archived
+                    </FormLabel>
+                    <FormDescription>
+                      This product will not appear anywhere in the store
+                    </FormDescription>
+                  </div>
                 </FormItem>
               )}
             />
