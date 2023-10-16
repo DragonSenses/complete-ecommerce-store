80c48d48330c9cfb971fab382050f0187ab6ae7d
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-16 06:36:25 -0700

    Update products GET route

--- ecommerce-admin/app/api/[storeId]/products/route.ts
+++ ecommerce-admin/app/api/[storeId]/products/route.ts
@@ -1,6 +1,7 @@
 // Global Imports
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
+import { requestToBodyStream } from "next/dist/server/body-streams";
 import { NextResponse } from "next/server";
 
 export async function POST(
@@ -109,15 +110,39 @@ export async function GET(
   { params }: { params: { storeId: string } }
 ) {
   try {
+    //  Access property of URL obj to access GET decoded query arguments
+    const { searchParams } = new URL(req.url);
+
+    // Extract parameters from query string to be used to filter products by
+    const categoryId = searchParams.get("categoryId") || undefined;
+    const colorId = searchParams.get("colorId") || undefined;
+    const sizeId = searchParams.get("sizeId") || undefined;
+    const isFeatured = searchParams.get("isFeatured");
+
     // Check if storeId exists
     if (!params.storeId) {
       return new NextResponse("Store ID is required", { status: 400 });
     }
 
-    // Find all products available in that store in the database
+    // Find all products in the store filtered by query arguments
+    // Never load archived products
     const products = await prismadb.product.findMany({
       where: {
-        storeId: params.storeId
+        storeId: params.storeId,
+        categoryId,
+        colorId,
+        sizeId,
+        isFeatured: isFeatured ? true : undefined,
+        isArchived: false
+      },
+      include: {
+        images: true,
+        category: true,
+        color: true,
+        size: true,
+      },
+      orderBy: {
+        createdAt: `desc`
       }
     });
 
