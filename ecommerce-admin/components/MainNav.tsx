a18506db0d96bf93f021fb5247f5b92c1c20e5c6
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-17 15:25:55 -0700

    Update MainNav.tsx
    - Add orders to routes

--- ecommerce-admin/components/MainNav.tsx
+++ ecommerce-admin/components/MainNav.tsx
@@ -42,6 +42,11 @@ export default function MainNav({
       active: pathname === `/${params.storeId}/products`,
     },
     {
+      href: `/${params.storeId}/orders`,
+      label: 'Orders',
+      active: pathname === `/${params.storeId}/orders`,
+    },
+    {
       href: `/${params.storeId}/sizes`,
       label: 'Sizes',
       active: pathname === `/${params.storeId}/sizes`,
