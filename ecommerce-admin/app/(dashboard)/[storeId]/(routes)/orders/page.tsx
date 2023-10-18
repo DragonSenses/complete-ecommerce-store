d681ec8a946a872102a0a5312a58d241689ded61
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-18 09:44:34 -0700

    Update OrdersPage
    - Include order items and individual products in the fetch
    - Format the orders in a more lucid style

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -6,6 +6,7 @@ import { format } from 'date-fns';
 // Local Imports
 import OrdersClient from './components/client';
 import { OrderColumn } from './components/columns';
+import { priceFormatter } from '@/lib/utils';
 
 const OrdersPage = async ({
   params
@@ -18,6 +19,13 @@ const OrdersPage = async ({
     where: {
       storeId: params.storeId
     },
+    include: {
+      orderItems: {
+        include: {
+          product: true
+        }
+      }
+    },
     orderBy: {
       createdAt: 'desc'
     }
@@ -26,10 +34,14 @@ const OrdersPage = async ({
   // Format each Order into a OrderColumn
   const formattedOrders: OrderColumn[] = orders.map((item) => ({
     id: item.id,
-    name: item.name,
-    value: item.value,
-    createdAt: format(item.createdAt, "MMMM do, yyyy"),
-  }))
+    phone: item.phone,
+    address: item.address,
+    products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
+    totalPrice: priceFormatter.format(item.orderItems.reduce((total, item) => {
+      return total + Number(item.product.price)
+    }, 0)),
+    createdAt: format(item.createdAt, "MMMM do, yyyy")
+  }));
 
   return (
     <div className='flex-col'>
