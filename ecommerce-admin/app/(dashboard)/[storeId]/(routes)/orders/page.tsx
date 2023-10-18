2806cb9cd20536aaa9b47b5168270326d0d2baf8
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-18 09:28:06 -0700

    Create page.tsx

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -0,0 +1,43 @@
+// Global Imports
+import React from 'react';
+import prismadb from '@/lib/prismadb';
+import { format } from 'date-fns';
+
+// Local Imports
+import OrdersClient from './components/client';
+import { OrderColumn } from './components/columns';
+
+const OrdersPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  
+  // Fetch all orders specific to the active store
+  const orders = await prismadb.order.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  // Format each Order into a OrderColumn
+  const formattedOrders: OrderColumn[] = orders.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }))
+
+  return (
+    <div className='flex-col'>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <OrdersClient data={formattedOrders}/>
+      </div>
+    </div>
+  );
+}
+
+export default OrdersPage;
