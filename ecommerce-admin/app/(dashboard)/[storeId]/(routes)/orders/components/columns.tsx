03065da326656e6e7e48f861b07cf314d3c738bc
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-19 12:29:46 -0700

    Create columns.tsx

---
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { CellAction } from "./cell-action"
+
+// This type is used to define the shape of our data.
+export type OrderColumn = {
+  id: string
+  name: string
+  value: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "value",
+    header: "Value",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />
+  },
+]
