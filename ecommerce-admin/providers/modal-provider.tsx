65515a71d4eca58f3b9de1bf045264c8656cd004
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-21 14:33:37 -0700

    docs: add comments to mounting logic

--- ecommerce-admin/providers/modal-provider.tsx
+++ ecommerce-admin/providers/modal-provider.tsx
@@ -7,17 +7,22 @@ import { useEffect, useState } from "react";
 import StoreModal from "@/components/modals/StoreModal";
 
 export const ModalProvider = () => {
+  // Declare isMounted state variable and initialize it to false
   const [isMounted, setIsMounted] = useState(false);
 
+  // useEffect hook to set isMounted variable to true
+  // Delays the execution of client-side-only code until after hydration
   useEffect(() => {
     setIsMounted(true);
-  }, []);
+  }, []); // Only run once after the initial render
 
-  if(!isMounted) {
+  // Prevent rendering of the component before the effect has run
+  // To protect from hydration errors or unwanted flashes of content
+  if (!isMounted) {
     return null;
   }
 
-  return(
+  return (
     <>
       <StoreModal />
     </>
