9fea977769160120fb34ca820369d3e93ccacf34
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-21 14:38:15 -0700

    feat: add navbar to output

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -4,6 +4,7 @@ import { Urbanist } from 'next/font/google';
 import './globals.css';
 
 import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
         <Footer />
       </body>
