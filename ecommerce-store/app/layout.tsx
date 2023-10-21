c639484d81ed1006be986aaeda4ea2d4afd8e663
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-20 17:29:37 -0700

    Add footer to output

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from 'next'
-import { Urbanist } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Urbanist } from 'next/font/google';
+
+import './globals.css';
+
+import Footer from '@/components/Footer';
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -16,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
