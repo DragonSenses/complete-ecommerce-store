81ee522db6e04c4c1db1c958179fcc968990705d
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-21 07:40:19 -0700

    Update Footer.tsx

--- ecommerce-store/components/Footer.tsx
+++ ecommerce-store/components/Footer.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export default function Footer() {
+
+  const date = new Date();
+
   return (
-    <div>Footer</div>
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; {date.getFullYear()} StoreName. All rights reserved.
+        </p>
+      </div>
+    </footer>
   )
 }
