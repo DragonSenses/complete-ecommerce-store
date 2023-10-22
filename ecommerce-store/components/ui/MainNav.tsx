0147c29b7ede0bb7c1b16e1677157fbab7fc21d8
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-22 13:46:10 -0700

    Update MainNav.tsx
    - get current URL pathname
    - map data to routes
    - style the nav

--- ecommerce-store/components/ui/MainNav.tsx
+++ ecommerce-store/components/ui/MainNav.tsx
@@ -1,3 +1,4 @@
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 // Define type and shape of props
@@ -8,8 +9,22 @@ interface MainNavProps {
 const MainNav: React.FC<MainNavProps> = ({
   data
 }) => {
+  // Hook that reads current URL pathname from a client component
+  // URL pathname comes after domain name and before query string
+  const pathname = usePathname();
+
+  // Map the data to usable route objects
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`
+  }));
+
+
   return (
-    <nav>
+    <nav
+      className="mx-6 flex items-center space-x-4 lg:space-x-6"
+    >
       MainNav
     </nav>
   )
