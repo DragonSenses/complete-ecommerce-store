133e514e0a223eb1be3bd75fbc7baf03d0b071f1
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-22 13:13:48 -0700

    Update Navbar.tsx

--- ecommerce-store/components/Navbar.tsx
+++ ecommerce-store/components/Navbar.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
+
+import Container from '@/components/ui/Container';
+import MainNav from '@/components/ui/MainNav';
 
 export default function Navbar() {
   return (
-    <nav className="border-b">
-      Navbar
-    </nav>
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+        <Link
+          className="ml-4 flex lg:ml-0 gap-x-2"
+          href="/"
+        >
+          <p className="font-bold text-xl">STORE</p>
+        </Link>
+        <MainNav />
+        </div>
+      </Container>
+    </div>
   )
 }
