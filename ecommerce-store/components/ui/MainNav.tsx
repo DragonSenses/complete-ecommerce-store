d760ac57f93c1a32dbe98839c75a802d876f7029
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-23 17:58:51 -0700

    Update type of data
    - Fix type of data for props
    - Fix issue "type void[] is not assignable to type ReactNode" found when mapping routes and returning an element in callback function

--- ecommerce-store/components/ui/MainNav.tsx
+++ ecommerce-store/components/ui/MainNav.tsx
@@ -3,10 +3,11 @@ import { usePathname } from 'next/navigation';
 import React from 'react';
 
 import { cn } from "@/lib/utils";
+import { Category } from '@/types';
 
 // Define type and shape of props
 interface MainNavProps {
-  data: any
+  data: Category[];
 }
 
 const MainNav: React.FC<MainNavProps> = ({
@@ -28,7 +29,7 @@ const MainNav: React.FC<MainNavProps> = ({
     <nav
       className="mx-6 flex items-center space-x-4 lg:space-x-6"
     >
-      {routes.map((route) => {
+      {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
@@ -39,7 +40,7 @@ const MainNav: React.FC<MainNavProps> = ({
         >
           {route.label}
         </Link>
-      })}
+      ))}
     </nav>
   )
 }
