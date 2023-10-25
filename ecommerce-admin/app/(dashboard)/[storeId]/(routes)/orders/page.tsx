c5820f3f40075f603586f9926aef30539a19a26b
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-24 18:08:41 -0700

    Update OrdersPage
    - Fixup OrderClient

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -47,7 +47,7 @@ const OrdersPage = async ({
   return (
     <div className='flex-col'>
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <OrdersClient data={formattedOrders}/>
+        <OrderClient data={formattedOrders}/>
       </div>
     </div>
   );
