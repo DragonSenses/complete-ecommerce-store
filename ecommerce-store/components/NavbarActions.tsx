a770e234503521e0027fb658735a566834a51754
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-25 18:42:47 -0700

    Update NavbarActions.tsx
    - Add button and icon to output
    - Add styles to output

--- ecommerce-store/components/NavbarActions.tsx
+++ ecommerce-store/components/NavbarActions.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import React from 'react';
+import { ShoppingBag } from 'lucide-react';
+
+import Button from '@/components/ui/Button';
 
 export default function NavbarActions() {
   return (
-    <div>NavbarActions</div>
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag 
+          size={20}
+          color="white"
+        />
+      </Button>
+    </div>
   )
 }
