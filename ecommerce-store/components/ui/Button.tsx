afaccd5f5bb61f846185989d9bac6ce4aba62bb0
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-25 18:48:17 -0700

    Enable overwrite of classes for custom styling

--- ecommerce-store/components/ui/Button.tsx
+++ ecommerce-store/components/ui/Button.tsx
@@ -1,5 +1,7 @@
 import React, { forwardRef } from "react";
 
+import { cn } from "@/lib/utils";
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
@@ -12,6 +14,23 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
 }, ref) => {
   return (
     <button
+      className={cn(
+        `
+        w-auto
+        rounded-full
+        bg-black
+        border-transparent
+        px-5
+        py-3
+        disabled:cursor-not-allowed
+        disabled:opacity-50
+        text-white
+        font-semibold
+        hover:opacity-75
+        transition
+        `,
+        className
+      )}
       ref={ref}
     >
       {children}
