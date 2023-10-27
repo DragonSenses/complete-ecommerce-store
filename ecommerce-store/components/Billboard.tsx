e9839713b8e08fb49a3183e14ed707350d52083f
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-26 20:14:25 -0700

    Update Billboard.tsx

--- ecommerce-store/components/Billboard.tsx
+++ ecommerce-store/components/Billboard.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-import { Billboard } from "@/types";
+import { Billboard as BillboardType } from "@/types";
 
-export default function Billboard() {
-  return (
-    <div>Billboard</div>
-  )
+interface BillboardProps {
+  data: BillboardType
 }
+
+const Billboard: React.FC<BillboardProps> = ({
+  data
+}) => {
+  return (
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
+
+    </div>
+  );
+}
+
+export default Billboard;
