102d7401762aebd35af2020a3a0fde119d0fd643
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-26 20:21:40 -0700

    Update home page output

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import Billboard from "@/components/Billboard";
+import Container from "@/components/ui/Container";
+
 export default function HomePage() {
   return(
-    <div>
-      Store
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={}/>
+      </div>
+    </Container>
   )
 }
