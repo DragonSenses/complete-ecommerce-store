ea37124b4b471242b18f05fad93003c02c421630
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-26 20:02:21 -0700

    Render after mounting to sync server & client
    - Prevent rendering of component until after it has been mounted to protect from hydration errors

--- ecommerce-store/components/NavbarActions.tsx
+++ ecommerce-store/components/NavbarActions.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ShoppingBag } from 'lucide-react';
 
 import Button from '@/components/ui/Button';
 
 export default function NavbarActions() {
+
+  // Declare isMounted state variable and initialize it to false
+  const [isMounted, setIsMounted] = useState(false);
+
+  // useEffect hook to set isMounted variable to true
+  // Delays the execution of client-side-only code until after hydration
+  useEffect(() => {
+    setIsMounted(true);
+  }, []); // Only run once after the initial render
+
+  // Prevent rendering of the component before the effect has run
+  // To protect from hydration errors or unwanted flashes of content
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center rounded-full bg-black px-4 py-2">
-        <ShoppingBag 
+        <ShoppingBag
           size={20}
           color="white"
         />
