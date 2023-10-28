95a7a3536f129c28d3a3058423b11e82601d571a
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-27 19:50:27 -0700

    Update getBillboard.tsx

--- ecommerce-store/actions/getBillboard.tsx
+++ ecommerce-store/actions/getBillboard.tsx
@@ -2,18 +2,18 @@ import { Billboard } from "@/types";
 
 // Dynamically build address to fetch a resource on the web
 // The scheme/protocol & domain name are defined as an environment variable
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
 /**
  * 
  * @returns an individual Billboard
  */
-const getCategories = async (): Promise<Billboard[]> => {
+const getBillboard = async (id: string): Promise<Billboard> => {
   // Send network request to the URL and save the response
-  const res = await fetch(URL);
+  const res = await fetch(`${URL}/${id}`);
 
   // Return the response in JSON format
   return res.json();
 }
 
-export default getCategories;
+export default getBillboard;
