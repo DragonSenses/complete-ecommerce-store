9ea93a3716b6dd51334e628b1b01057846f2d34f
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-28 14:52:18 -0700

    Add product interface

--- ecommerce-store/types.ts
+++ ecommerce-store/types.ts
@@ -9,3 +9,14 @@ export interface Category {
   name: string;
   billboard: Billboard;
 };
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  color: Color;
+  images: Image[]
+}
