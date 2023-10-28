8c3dc7fcdcf44eb95c5204cbbe58363ca57fb27b
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-27 18:38:07 -0700

    Add revalidation on every request

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,11 +1,15 @@
 import Billboard from "@/components/Billboard";
 import Container from "@/components/ui/Container";
 
+// Revalidate the page on every request, purge cache & fetch latest data
+export const revalidate = 0;
+
 export default function HomePage() {
-  return(
+
+  return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={}/>
+        <Billboard data={ } />
       </div>
     </Container>
   )
