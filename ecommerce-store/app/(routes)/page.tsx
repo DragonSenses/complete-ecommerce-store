21157424fc63ff62301648025e0dc2375fc882af
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-27 20:15:41 -0700

    Update HomePage
    - fetch the billboard and pass in data

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,15 +1,18 @@
+import getBillboard from "@/actions/getBillboard";
 import Billboard from "@/components/Billboard";
 import Container from "@/components/ui/Container";
 
 // Revalidate the page on every request, purge cache & fetch latest data
 export const revalidate = 0;
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const billboard = await getBillboard();
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={ } />
+        <Billboard data={billboard} />
       </div>
     </Container>
   )
