0bfb2c9250b9c2cc4208bf2e6bab450a532cc435
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-28 18:46:29 -0700

    Add query string to URL to filter data

--- ecommerce-store/actions/getProducts.tsx
+++ ecommerce-store/actions/getProducts.tsx
@@ -1,14 +1,34 @@
+import queryString from "query-string"
+
 import { Product } from "@/types";
 
 // Dynamically build address to fetch a resource on the web
 // The scheme/protocol & domain name are defined as an environment variable
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
+interface Query {
+  categoryId?: string;
+  colorId?: string;
+  sizeId?: string;
+  isFeatured?: boolean;
+}
+
 /**
  * 
  * @returns an array of products
  */
-const getProducts = async (): Promise<Product[]> => {
+const getProducts = async (query: Query): Promise<Product[]> => {
+  // Stringify an object into a URL with a query string and sorting the keys
+  const url = queryString.stringifyUrl({
+    url: URL,
+    query: {
+      colorId: query.colorId,
+      sizeId: query.sizeId,
+      categoryId: query.categoryId,
+      isFeatured: query.isFeatured,
+    },
+  })
+
   // Send network request to the URL and save the response
   const res = await fetch(URL);
 
