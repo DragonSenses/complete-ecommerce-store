fbdd1b64df07f6b98fa128db18d2d4286ad9181e
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-29 17:33:11 -0700

    Update page.tsx

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import getBillboard from "@/actions/getBillboard";
+import getProducts from "@/actions/getProducts";
 import Billboard from "@/components/Billboard";
 import Container from "@/components/ui/Container";
 
@@ -7,13 +8,18 @@ export const revalidate = 0;
 
 export default async function HomePage() {
 
-  const billboard = await getBillboard();
+  // Fetch featured products
+  const products = await getProducts({isFeatured: true});
+
+  // Fetch billboard
+  const billboard = await getBillboard("5129787f-d293-45dc-9c72-e6fe2e290ca9");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
       </div>
+
     </Container>
   )
 }
