9d5244dde6f28c256f924e9577590b6b7e41af9f
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-30 21:28:37 -0700

    Add conditional render when items are empty

--- ecommerce-store/components/ProductList.tsx
+++ ecommerce-store/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Product } from '@/types';
+import NoResults from '@/components/ui/NoResults';
 
 interface ProductListProps {
   title: string;
@@ -14,6 +15,7 @@ const ProductList: React.FC<ProductListProps> = ({
   return (
     <div className="space-y-4">
       <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResults />}
     </div>
   )
 }
