201cbf7c7d4ddde698877054192613fd96700306
DragonSenses <kendrickfrancisco@gmail.com>
2023-10-30 22:22:00 -0700

    Add mapped products to output

--- ecommerce-store/components/ProductList.tsx
+++ ecommerce-store/components/ProductList.tsx
@@ -16,6 +16,13 @@ const ProductList: React.FC<ProductListProps> = ({
     <div className="space-y-4">
       <h3 className="font-bold text-3xl">{title}</h3>
       {items.length === 0 && <NoResults />}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <div key={item.id}>
+            {item.name}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
