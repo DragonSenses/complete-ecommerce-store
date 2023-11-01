e250faef2d9ee3f9dd2fec636d4b79d632fce545
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-01 03:15:29 -0700

    Update ProductCard output
    - Add background container
    - Add image container
    - Add product image

--- ecommerce-store/components/ui/ProductCard.tsx
+++ ecommerce-store/components/ui/ProductCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from 'next/image';
 import React from 'react';
 
 import { Product } from '@/types';
@@ -12,8 +13,15 @@ const ProductCard: React.FC<ProductCard> = ({
   data
 }) => {
   return (
-    <div>
-      Product Card
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      {/* Images & Actions */}
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image 
+          src={data?.images?.[0]?.url}
+          fill
+          alt="Product Image"
+        />
+      </div>
     </div>
   )
 }
