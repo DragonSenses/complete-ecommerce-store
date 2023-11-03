0b0160388dfeb8e3e7354c21c715c3b8df91d484
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-03 05:30:07 -0700

    Delay client-side rendering until after hydration

--- ecommerce-store/components/ui/Currency.tsx
+++ ecommerce-store/components/ui/Currency.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
 
 const priceFormatter = new Intl.NumberFormat("en-US", {
   style: 'currency',
@@ -12,6 +14,21 @@ interface CurrencyProps {
 const Currency: React.FC<CurrencyProps> = ({
   value
 }) => {
+  // Declare isMounted state variable and initialize it to false
+  const [isMounted, setIsMounted] = useState(false);
+
+  // useEffect hook to set isMounted variable to true
+  // Delays the execution of client-side-only code until after hydration
+  useEffect(() => {
+    setIsMounted(true);
+  }, []); // Only run once after the initial render
+
+  // Prevent rendering of the component before the effect has run
+  // To protect from hydration errors or unwanted flashes of content
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div className="font-semibold">
       {priceFormatter.format(Number(value))}
