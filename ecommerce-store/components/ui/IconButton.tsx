3dd6f9474bb511ea04763032d285f472cc95a0b8
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-02 20:15:26 -0700

    Add styles to icon button

--- ecommerce-store/components/ui/IconButton.tsx
+++ ecommerce-store/components/ui/IconButton.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import { cn } from '@/lib/utils';
+
 const IconButton = () => {
   return (
-    <div>IconButton</div>
+    <button
+      onClick={onClick}
+      className={cn(
+        "rounded-full flex items-center justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
+        className
+      )}
+    >
+
+    </button>
   )
 }
 
