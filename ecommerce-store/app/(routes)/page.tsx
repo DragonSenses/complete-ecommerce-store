1cc1087a06a3ffa6430a8771dfc0a88151d74f26
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-07 03:45:19 -0800

    Update home page output
    Wrap both elements inside the same container

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -10,7 +10,7 @@ export const revalidate = 0;
 export default async function HomePage() {
 
   // Fetch featured products
-  const products = await getProducts({isFeatured: true});
+  const products = await getProducts({ isFeatured: true });
 
   // Fetch billboard
   const billboard = await getBillboard("5129787f-d293-45dc-9c72-e6fe2e290ca9");
@@ -19,9 +19,9 @@ export default async function HomePage() {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" items={products}/>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   )
