69777f535f6b3e4e4ddf9676ec0d3a4d17aab0bb
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-08 05:23:49 -0800

    Update product page
    - Convert to async
    - Fetch suggested products

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
+import getProducts from '@/actions/getProducts';
+
 interface ProductPageProps {
   params: {
     productId: string;
   }
 }
 
-const ProductPage: React.FC<ProductPageProps> = ({
+const ProductPage: React.FC<ProductPageProps> = async ({
   params
 }) => {
+
+  const product = {};
+
+  const suggestedProducts = await getProducts({
+    categoryId: product?.category?.id;
+  })
+
   return (
     <div>Individual Product Page</div>
   )
