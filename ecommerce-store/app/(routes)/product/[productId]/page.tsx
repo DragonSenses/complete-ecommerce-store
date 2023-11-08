4a8c3695bec623987469cd36f1790c985668642a
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-08 06:01:20 -0800

    Update product page
    - fetch the necessary data such as product and products in related category

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import getProduct from '@/actions/getProduct';
 import getProducts from '@/actions/getProducts';
 
 interface ProductPageProps {
@@ -11,11 +12,12 @@ interface ProductPageProps {
 const ProductPage: React.FC<ProductPageProps> = async ({
   params
 }) => {
+  // Fetch the individual product
+  const product = await getProduct(params.productId);
 
-  const product = {};
-
+  // Fetch suggested products using the same category
   const suggestedProducts = await getProducts({
-    categoryId: product?.category?.id;
+    categoryId: product?.category?.id
   })
 
   return (
