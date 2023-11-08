6d558c617a8228f8b13fb72c34310c91c5447246
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-08 06:16:13 -0800

    Update product page output
    - Add containers and divisions to content
    - Added gallery and info to page

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import getProduct from '@/actions/getProduct';
 import getProducts from '@/actions/getProducts';
+import Container from '@/components/ui/Container';
 
 interface ProductPageProps {
   params: {
@@ -21,7 +22,20 @@ const ProductPage: React.FC<ProductPageProps> = async ({
   })
 
   return (
-    <div>Individual Product Page</div>
+    <div className='bg-white'>
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            {/* Gallery */}
+            <div>Gallery</div>
+            <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
+              {/* Info */}
+              Product Info
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
