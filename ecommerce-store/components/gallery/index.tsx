2c7455350ae1d5b7ddf3c152f18e9699945fb299
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-10 05:03:00 -0800

    Map each image to GalleryTab

--- ecommerce-store/components/gallery/index.tsx
+++ ecommerce-store/components/gallery/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Tab } from "@headlessui/react";
 
 import { Image as ImageType } from "@/types";
+import GalleryTab from './GalleryTab';
 
 interface GalleryProps {
   images: ImageType[];
@@ -18,7 +19,7 @@ const Gallery: React.FC<GalleryProps> = ({
       <div className='mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none'>
         <Tab.List className='grid grid-cols-4 gap-6'>
           {images.map((image) => (
-            <Tab key={image.id} image={image}/>
+            <GalleryTab key={image.id} image={image}/>
           ))}
         </Tab.List>
       </div>
