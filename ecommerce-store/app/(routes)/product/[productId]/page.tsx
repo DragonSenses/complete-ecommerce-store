a479d3e936e48707b97fe32513ebc7a37cce24cc
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-09 18:19:30 -0800

    Update product page with gallery
    Render Gallery component in output

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -4,6 +4,7 @@ import getProduct from '@/actions/getProduct';
 import getProducts from '@/actions/getProducts';
 import Container from '@/components/ui/Container';
 import ProductList from '@/components/ProductList';
+import Gallery from '@/components/gallery';
 
 interface ProductPageProps {
   params: {
@@ -27,8 +28,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            {/* Gallery */}
-            <div>Gallery</div>
+            <Gallery />
             <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
               {/* Info */}
               Product Info
