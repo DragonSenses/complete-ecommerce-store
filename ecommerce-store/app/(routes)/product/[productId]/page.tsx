255e29c022aa0cd030fd16aa97646ce7956d6db2
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-09 20:18:28 -0800

    Pass data to gallery component
    Pass product images to the Gallery component.

--- ecommerce-store/app/(routes)/product/[productId]/page.tsx
+++ ecommerce-store/app/(routes)/product/[productId]/page.tsx
@@ -28,7 +28,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            <Gallery />
+            <Gallery images={product.images}/>
             <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
               {/* Info */}
               Product Info
