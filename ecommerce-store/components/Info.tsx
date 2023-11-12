a49693fe8aa5ba7c02816a3dead5f682336604c2
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-12 10:24:22 -0800

    Use Currency component to display product price

--- ecommerce-store/components/Info.tsx
+++ ecommerce-store/components/Info.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Product } from '@/types';
+import Currency from '@/components/ui/Currency';
 
 interface InfoProps {
   data: Product;
@@ -14,6 +15,17 @@ const Info: React.FC<InfoProps> = ({
       <h1 className='text-3xl font-bold text-gray-900'>
         {data.name}
       </h1>
+
+      <div className='mt-3 flex items-end justify-between'>
+        <p className='text-2xl text-gray-900'>
+          <Currency value={data?.price} />
+        </p>
+      </div>
+      <hr className='my-4'/>
+      
+      <div>
+
+      </div>
     </div>
   )
 }
