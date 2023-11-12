3f29649557cbe3ce360f4bbda1f72f7ced714c33
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-12 10:15:17 -0800

    Add Info component to display product name
    - Add styles to header that displays product name

--- ecommerce-store/components/Info.tsx
+++ ecommerce-store/components/Info.tsx
@@ -11,7 +11,7 @@ const Info: React.FC<InfoProps> = ({
 }) => {
   return (
     <div>
-      <h1 className=''>
+      <h1 className='text-3xl font-bold text-gray-900'>
         {data.name}
       </h1>
     </div>
