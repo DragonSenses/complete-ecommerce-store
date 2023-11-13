6f0c02fee2036e812fd9bf5100628bd23a8482d6
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-13 06:59:33 -0800

    Display product color in Info component

--- ecommerce-store/components/Info.tsx
+++ ecommerce-store/components/Info.tsx
@@ -29,10 +29,29 @@ const Info: React.FC<InfoProps> = ({
           Size:
         </h3>
         <div>
-            {data?.size.name}
+            {data?.size?.name}
         </div>
       </div>
-      
+
+      <div className='flex items-center gap-x-4'>
+        <h3 className='font-semibold text-black'>
+          Size:
+        </h3>
+        <div>
+            {data?.size?.name}
+        </div>
+      </div>
+
+      <div className='flex items-center gap-x-4'>
+        <h3 className='font-semibold text-black'>
+          Color:
+        </h3>
+        <div 
+          className='h-6 w-6 rounded-full border-gray-600'
+          style={{ backgroundColor: data?.color?.value }}  
+        />
+      </div>
+
     </div>
   )
 }
