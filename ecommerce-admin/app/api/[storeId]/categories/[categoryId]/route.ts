af83bff7d297b6a89e141c8e7be5106566667a8b
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-15 16:00:06 -0800

    Include billboard data in category GET response

--- ecommerce-admin/app/api/[storeId]/categories/[categoryId]/route.ts
+++ ecommerce-admin/app/api/[storeId]/categories/[categoryId]/route.ts
@@ -16,6 +16,9 @@ export async function GET (
     const category = await prismadb.category.findUnique({
       where: {
         id: params.categoryId,
+      },
+      include: {
+        billboard: true,
       }
     });
 
