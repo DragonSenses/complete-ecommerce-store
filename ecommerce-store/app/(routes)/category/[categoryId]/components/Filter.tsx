6a1347c3ce0286fc9e7bc4614160808feed1cd72
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-16 19:16:25 -0800

    Use Next.js hooks to get selected value of query

--- ecommerce-store/app/(routes)/category/[categoryId]/components/Filter.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/components/Filter.tsx
@@ -1,5 +1,9 @@
-import { Color, Size } from '@/types';
+"use client";
+
 import React from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import { Color, Size } from '@/types';
 
 interface FilterProps {
   data: (Size | Color)[];
@@ -12,6 +16,17 @@ const Filter: React.FC<FilterProps> = ({
   name,
   valueKey
 }) => {
+  // URLSearchParams interface to read & update the query string of current URL
+  const searchParams = useSearchParams();
+
+  // object that contains current route info & methods to manipulate app router
+  const router = useRouter();
+
+  // Get the currently selected value of the query parameter
+  const selectedValue = searchParams.get(valueKey);
+
+  // onClick function that accepts id
+
   return (
     <div>Filter</div>
   )
