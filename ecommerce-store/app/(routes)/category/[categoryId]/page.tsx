da16b576034fb4bbc1f23aa890db970842fab359
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-18 06:44:27 -0800

    Add responsive grid to display products

--- ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
+++ ecommerce-store/app/(routes)/category/[categoryId]/page.tsx
@@ -61,6 +61,13 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             </div>
             <div className='mt-6 lg:col-span-4 lg:mt-0'>
               {products.length == 0 && <NoResults />}
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                {products.map((item) => (
+                  <div>
+
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
