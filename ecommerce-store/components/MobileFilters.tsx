7d09653ca7c2b9a3b6f405ec5e3575326273d5f3
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-20 12:20:06 -0800

    Add Dialog.Panel and close button

--- ecommerce-store/components/MobileFilters.tsx
+++ ecommerce-store/components/MobileFilters.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
-import { Plus } from 'lucide-react';
+import { Plus, X } from 'lucide-react';
 import { Dialog } from '@headlessui/react';
 
 import { Color, Size } from '@/types';
 import Button from '@/components/ui/Button';
+import IconButton from './ui/IconButton';
 
 interface MobileFiltersProps {
   sizes: Size[];
   colors: Color[];
 }
 
-const MobileFilters:React.FC<MobileFiltersProps> = ({
+const MobileFilters: React.FC<MobileFiltersProps> = ({
   sizes,
   colors
 }) => {
@@ -28,7 +29,21 @@ const MobileFilters:React.FC<MobileFiltersProps> = ({
 
       <Dialog open={open} onClose={onClose} as='div' className='relative z-40 lg:hidden'>
         {/* Background */}
-        {/* Dialog Poosition */}
+        <div className='fixed inset-0 bg-black bg-opacity-25' />
+
+        {/* Dialog Position */}
+        <div className='fixed inset-0 z-40 flex'>
+          <Dialog.Panel className='relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl'>
+
+            {/* Close Button */}
+            <div className='flex items-cetner justify-end px-4'>
+              <IconButton icon={<X size={15} />} onClick={onClose} />
+            </div>
+
+            
+
+          </Dialog.Panel>
+        </div>
       </Dialog>
     </>
   )
