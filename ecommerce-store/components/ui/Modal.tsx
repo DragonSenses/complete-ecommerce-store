5cf1bd305b1fd0576f22d44a99cb36fe0bcd427b
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-21 17:33:26 -0800

    refactor: add ModalProps interface

--- ecommerce-store/components/ui/Modal.tsx
+++ ecommerce-store/components/ui/Modal.tsx
@@ -2,7 +2,17 @@
 
 import React from 'react';
 
-const Modal = () => {
+interface ModalProps {
+  open: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  open,
+  onClose,
+  children,
+}) => {
   return (
     <div>Modal</div>
   )
