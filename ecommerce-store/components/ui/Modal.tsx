733d72014a69679615ddb7698201da5ccd71dc23
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-22 18:38:42 -0800

    Add Dialog component to Modal

--- ecommerce-store/components/ui/Modal.tsx
+++ ecommerce-store/components/ui/Modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { Fragment } from 'react';
-import { Transition } from '@headlessui/react';
+import { Dialog, Transition } from '@headlessui/react';
 
 interface ModalProps {
   open: boolean;
@@ -16,7 +16,19 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
   return (
     <Transition show={open} as={Fragment}>
+      <Dialog as="div" className='relative z-10' onClose={onClose}>
+        <div className="fixed inset-0 bg-black bg-opacity-50" />
 
+        <div className='fixed inset-0 overflow-y-auto'>
+          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+            <Transition.Child
+              as={Fragment}
+            >
+
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
     </Transition>
   )
 }
