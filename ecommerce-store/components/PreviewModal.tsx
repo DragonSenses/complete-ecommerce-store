a0c4ec1b092507f3277c23e4eaa44db8a8ecc708
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-24 21:28:17 -0800

    Use custom hook to access state of PreviewModal

--- ecommerce-store/components/PreviewModal.tsx
+++ ecommerce-store/components/PreviewModal.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import React from 'react';
 
+import usePreviewModal from '@/hooks/use-preview-modal';
+
 const PreviewModal = () => {
+  const previewModal = usePreviewModal();
+
+  // Use custom hook to access the state of PreviewModal to display product info
+  const product = usePreviewModal((state) => state.data);
+
+
   return (
     <div>PreviewModal</div>
   )
