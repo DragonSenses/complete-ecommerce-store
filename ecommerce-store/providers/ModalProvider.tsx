f1cbf0132e9e07f8ca6a35572e776204fdc95071
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-27 15:30:50 -0800

    Add isMounted state & useEffect hook
    - Add isMounted state variable and initialize it to false
    - Add useEffect hook to set isMounted variable to true after hydration
    - Add conditional rendering to prevent hydration errors or unwanted flashes of content

--- ecommerce-store/providers/ModalProvider.tsx
+++ ecommerce-store/providers/ModalProvider.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import PreviewModal from '@/components/PreviewModal';
 
 const ModalProvider = () => {
+  // Declare isMounted state variable and initialize it to false
+  const [isMounted, setIsMounted] = useState(false);
+
+  // useEffect hook to set isMounted variable to true
+  // Delays the execution of client-side-only code until after hydration
+  useEffect(() => {
+    setIsMounted(true);
+  }, []); // Only run once after the initial render
+
+  // Prevent rendering of the component before the effect has run
+  // To protect from hydration errors or unwanted flashes of content
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <>
       <PreviewModal />
