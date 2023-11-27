8f5a4f2004ee6779c2a3fe07cffca0cb33095f5c
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-26 19:23:19 -0800

    Pass in product image data to Gallery component

--- ecommerce-store/components/PreviewModal.tsx
+++ ecommerce-store/components/PreviewModal.tsx
@@ -22,10 +22,9 @@ const PreviewModal = () => {
       open={previewModal.isOpen}
       onClose={previewModal.onClose}
     >
-      {/* Children */}
       <div className='grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8'>
         <div className='sm:col-span-4 lg:col-span-5'>
-          <Gallery />
+          <Gallery images={product.images} />
         </div>
         <div className='sm:col-span-8 lg:col-span-7'>
 
