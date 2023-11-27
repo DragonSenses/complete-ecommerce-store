f4415c9ae4897b0ce42599cb678f427ad56e6a98
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-26 19:21:08 -0800

    Add Gallery component to PreviewModal

--- ecommerce-store/components/PreviewModal.tsx
+++ ecommerce-store/components/PreviewModal.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 import usePreviewModal from '@/hooks/use-preview-modal';
 import Modal from '@/components/ui/Modal';
+import Gallery from '@/components/gallery';
 
 const PreviewModal = () => {
   const previewModal = usePreviewModal();
@@ -21,8 +22,14 @@ const PreviewModal = () => {
       open={previewModal.isOpen}
       onClose={previewModal.onClose}
     >
+      {/* Children */}
       <div className='grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8'>
-        
+        <div className='sm:col-span-4 lg:col-span-5'>
+          <Gallery />
+        </div>
+        <div className='sm:col-span-8 lg:col-span-7'>
+
+        </div>
       </div>
     </Modal>
   )
