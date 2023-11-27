cf28472a62f8212f67dc6b322c2c6f15c1be4a8a
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-26 19:32:08 -0800

    Add PreviewModal component to ModalProvider

--- ecommerce-store/providers/ModalProvider.tsx
+++ ecommerce-store/providers/ModalProvider.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import React from 'react';
+
+import PreviewModal from '@/components/PreviewModal';
 
 const ModalProvider = () => {
   return (
-    <div>ModalProvider</div>
+    <>
+      <PreviewModal />
+    </>
   )
 }
 
