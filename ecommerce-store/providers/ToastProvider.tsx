6a88f26854ac5da6b56bafc16c705398758c4fde
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-28 19:56:35 -0800

    Add ToastProvider component using react-hot-toast

--- ecommerce-store/providers/ToastProvider.tsx
+++ ecommerce-store/providers/ToastProvider.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
+/**
+ * Defines a functional component that serves as a provider, 
+ * to pass data to any nested components that need to access it. 
+ * @returns a Toaster element
+ */
 const ToastProvider = () => {
   return (
-    <div>ToastProvider</div>
+    // Render the Toaster element with default props
+    <Toaster />
   )
 }
 
+// Export the ToastProvider component
 export default ToastProvider
