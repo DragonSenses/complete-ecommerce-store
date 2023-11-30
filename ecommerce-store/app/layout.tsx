8d8d09cb73bea99cf39013965a14fc9d8da5d498
DragonSenses <kendrickfrancisco@gmail.com>
2023-11-29 18:35:10 -0800

    Add ToastProvider to RootLayout

--- ecommerce-store/app/layout.tsx
+++ ecommerce-store/app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import ModalProvider from '@/providers/ModalProvider';
+import ToastProvider from '@/providers/ToastProvider';
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
