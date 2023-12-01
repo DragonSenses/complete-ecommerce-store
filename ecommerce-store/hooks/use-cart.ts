2cae6fa7ce5326703ec190fc265b4f2fef2fc7d5
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-01 15:58:57 -0800

    Fix expression is not callable error in useCart
    - Use the create function from zustand instead of the default import
    to avoid the TypeScript error that occurs when using persist middleware.
    - Add toast notifications for adding and removing items from the cart.
    - Indent code to improve readability

--- ecommerce-store/hooks/use-cart.ts
+++ ecommerce-store/hooks/use-cart.ts
@@ -1,43 +1,47 @@
-import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import toast from "react-hot-toast";
 
-import { Product } from '@/types';
+import { Product } from "@/types";
 
 interface CartStore {
   items: Product[];
   addItem: (data: Product) => void;
   removeItem: (id: string) => void;
   removeAll: () => void;
-};
+}
 
 const useCart = create(
   persist<CartStore>(
     (set, get) => ({
-    items: [],
-    addItem: (data: Product) => {
-      // Get the current state of items
-      const currentItems = get().items;
-      
-      // Check if user already has an existing item in the cart
-      const existingItem = currentItems.find((item) => item.id === data.id);
+      items: [],
+      addItem: (data: Product) => {
+        // Get the current state of items
+        const currentItems = get().items;
 
-      if (existingItem) {
-        return toast("Item already in cart.");
-      }
+        // Check if user already has an existing item in the cart
+        const existingItem = currentItems.find((item) => item.id === data.id);
 
-      set({ items: [...get().items, data] });
-      toast.success("Item added to cart.")
-    },
-    removeItem: (id: string) => {
-      set({ items: [...get().items.filter((item) => item.id !== id)] });
-      toast.success("Item removed from the cart.")
-    },
-    removeAll(): () => set({ items: [] }),
-  }), 
-  {
-    name: 'cart-storage',
-    storage: createJSONStorage(() => localStorage),
-  }),
+        if (existingItem) {
+          return toast("Item already in cart.");
+        }
+
+        // Add new item to cart by setting the state
+        set({ items: [...get().items, data] });
+        toast.success("Item added to cart.");
+      },
+      removeItem: (id: string) => {
+        // Filter items by id
+        set({ items: [...get().items.filter((item) => item.id !== id)] });
+        toast.success("Item removed from the cart.");
+      },
+      removeAll: () => set({ items: [] }),
+    }),
+    {
+      name: "cart-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
 
 export default useCart;
