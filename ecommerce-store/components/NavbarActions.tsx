573a27105899980d68f664d9577bdb51125cc6d3
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-02 19:05:03 -0800

    Add button to navigate to cart page from navbar
    - Create a button component that displays the shopping bag icon and the number of items in the cart.
    - Use the router object to push the cart page path when the button is clicked.

--- ecommerce-store/components/NavbarActions.tsx
+++ ecommerce-store/components/NavbarActions.tsx
@@ -31,7 +31,10 @@ export default function NavbarActions() {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button 
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingBag
           size={20}
           color="white"
