1af6360d0cf0402ae915976daaa88061207ba18c
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-04 20:18:32 -0800

    Add useCart hook to display cart items in CartPage
    - Import useCart hook from '@/hooks/use-cart'
    - Use useCart hook to access cart state and items
    - Add a conditional rendering to display a message if cart is empty
    - Add a ul element to display cart items

--- ecommerce-store/app/(routes)/cart/page.tsx
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -30,7 +30,18 @@ const CartPage = () => {
           <h1 className='text-3xl font-bold text-black'>Shopping Cart</h1>
           <div className='mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12'>
             <div className='lg:col-span-7'>
-
+              {cart.items.length === 0 &&
+                <p className='text-neutral-500'>No items added to cart</p>
+              }
+              <ul>
+                {cart.items.map((item) => (
+                  <li
+                    key={item.id}
+                  >
+                    {item.name}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
