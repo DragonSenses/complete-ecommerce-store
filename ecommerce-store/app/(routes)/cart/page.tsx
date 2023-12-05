6de94d8a13723ffdd82ae4b78916a99da2d918cb
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-04 20:10:01 -0800

    Add grid layout & style to title in CartPage

--- ecommerce-store/app/(routes)/cart/page.tsx
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -3,8 +3,10 @@
 import React, { useEffect, useState } from 'react';
 
 import Container from '@/components/ui/Container';
+import useCart from '@/hooks/use-cart';
 
 const CartPage = () => {
+  const cart = useCart();
 
   // Declare isMounted state variable and initialize it to false
   const [isMounted, setIsMounted] = useState(false);
@@ -25,7 +27,12 @@ const CartPage = () => {
     <div className='bg-white'>
       <Container>
         <div className='px-4 py-16 sm:px-6 lg:px-8'>
-          <h1>Shopping Cart</h1>
+          <h1 className='text-3xl font-bold text-black'>Shopping Cart</h1>
+          <div className='mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12'>
+            <div className='lg:col-span-7'>
+
+            </div>
+          </div>
         </div>
       </Container>
     </div>
