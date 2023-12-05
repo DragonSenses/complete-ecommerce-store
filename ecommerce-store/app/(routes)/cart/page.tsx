a407cb95baa457027b554b47e284c648d9b6be02
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-04 19:48:28 -0800

    Add isMounted state & useEffect hook to CartPage
    - Mark as client component
    - Use useState hook to declare and initialize isMounted state variable to false
    - Use useEffect hook to set isMounted state variable to true after hydration
    - Return null if isMounted is false to prevent rendering before the effect has run
    - Protect from hydration errors or unwanted flashes of content

--- ecommerce-store/app/(routes)/cart/page.tsx
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
 
 const CartPage = () => {
+
+  // Declare isMounted state variable and initialize it to false
+  const [isMounted, setIsMounted] = useState(false);
+
+  // useEffect hook to set isMounted variable to true
+  // Delays the execution of client-side-only code until after hydration
+  useEffect(() => {
+    setIsMounted(true);
+  }, []); // Only run once after the initial render
+
+  // Prevent rendering of the component before the effect has run
+  // To protect from hydration errors or unwanted flashes of content
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <div>
       CartPage
