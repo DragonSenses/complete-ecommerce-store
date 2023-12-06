dd85d8158f19ba17d40c540a3508a20de72081d8
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-05 18:29:14 -0800

    Update CartItem.tsx
    - Add "use client"
    - Add global and local imports

--- ecommerce-store/app/(routes)/cart/components/CartItem.tsx
+++ ecommerce-store/app/(routes)/cart/components/CartItem.tsx
@@ -1,4 +1,14 @@
+"use client";
+
 import React from 'react';
+import Image from 'next/image';
+import { toast } from 'react-hot-toast';
+import { X } from 'lucide-react';
+
+import IconButton from '@/components/ui/IconButton';
+import Currency from '@/components/ui/Currency';
+import useCart from '@/hooks/use-cart';
+import { Product } from '@/types';
 
 const CartItem = () => {
   return (
