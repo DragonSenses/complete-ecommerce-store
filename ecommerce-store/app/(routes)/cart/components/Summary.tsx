f478f878541c37dbb0de31f54fb5c77061bb4781
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-08 17:26:36 -0800

    Add local imports to Summary component

--- ecommerce-store/app/(routes)/cart/components/Summary.tsx
+++ ecommerce-store/app/(routes)/cart/components/Summary.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+// Global Imports
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
+import { toast } from 'react-hot-toast';
+
+// Local Imports
+import Button from '@/components/ui/Button';
+import Currency from '@/components/ui/Currency';
+import useCart from '@/hooks/use-cart';
 
 const Summary = () => {
   return (
