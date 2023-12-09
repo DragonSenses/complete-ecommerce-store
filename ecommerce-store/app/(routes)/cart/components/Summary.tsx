bee5c7dc6f25e5e077d1704923c2097b4d26117c
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-08 17:41:21 -0800

    Make Summary responsive and user-friendly
    - Add responsive padding and margins to Summary
    - Specify font color, size & weight to make text more legible
    - Add an h2 title
    - Allow component to span 5 columns on large screens (width of 1024 pixels or greater)

--- ecommerce-store/app/(routes)/cart/components/Summary.tsx
+++ ecommerce-store/app/(routes)/cart/components/Summary.tsx
@@ -13,7 +13,11 @@ import useCart from '@/hooks/use-cart';
 
 const Summary = () => {
   return (
-    <div>Summary</div>
+    <div className='mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:pt-8'>
+      <h2 className='text-lg font-medium text-gray-900'>
+        Order Summary
+      </h2>
+    </div>
   )
 }
 
