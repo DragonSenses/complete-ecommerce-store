92f52f78b94c1e16394dcb1380d8f1c5a778201a
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-08 17:19:09 -0800

    Add Summary component to cart page

--- ecommerce-store/app/(routes)/cart/page.tsx
+++ ecommerce-store/app/(routes)/cart/page.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 import Container from '@/components/ui/Container';
 import useCart from '@/hooks/use-cart';
 import CartItem from './components/CartItem';
+import Summary from './components/Summary';
 
 const CartPage = () => {
   const cart = useCart();
@@ -30,6 +31,7 @@ const CartPage = () => {
         <div className='px-4 py-16 sm:px-6 lg:px-8'>
           <h1 className='text-3xl font-bold text-black'>Shopping Cart</h1>
           <div className='mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12'>
+
             <div className='lg:col-span-7'>
               {cart.items.length === 0 &&
                 <p className='text-neutral-500'>No items added to cart</p>
@@ -43,6 +45,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
+            <Summary />
           </div>
         </div>
       </Container>
