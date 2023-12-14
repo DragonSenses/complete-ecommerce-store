3e536bf4ef3cc6a25d4a2f9ebbfa79e5752c704e
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-13 18:35:24 -0800

    Add stripe API information

--- ecommerce-admin/lib/stripe.ts
+++ ecommerce-admin/lib/stripe.ts
@@ -1,3 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe("YOUR_STRIPE_API_KEY");
+export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+  apiVersion: "2022-11-15",
+  typescript: true,
+});
