54c8657dcb5aaf3956f249505313f331f41f2afd
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-14 21:30:13 -0800

    Add preflight OPTIONS request to checkout route
    - Add the OPTIONS request to check if cross-origin request is allowed

--- ecommerce-admin/app/api/[storeId]/checkout/route.ts
+++ ecommerce-admin/app/api/[storeId]/checkout/route.ts
@@ -4,8 +4,13 @@ import { NextResponse } from 'next/server';
 import { stripe } from '@/lib/stripe';
 import prismadb from '@/lib/prismadb';
 
+// Configure the CORS policy by setting HTTP response headers
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
