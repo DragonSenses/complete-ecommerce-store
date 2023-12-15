94c092138eb5acf7da1513f56f6b6df7f88f633b
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-14 21:16:17 -0800

    Configure CORS policy in checkout route

--- ecommerce-admin/app/api/[storeId]/checkout/route.ts
+++ ecommerce-admin/app/api/[storeId]/checkout/route.ts
@@ -4,3 +4,8 @@ import { NextResponse } from 'next/server';
 import { stripe } from '@/lib/stripe';
 import prismadb from '@/lib/prismadb';
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
