350e420ff0370cd83beacaaf8b65fbdc5ee9abde
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-17 22:09:34 -0800

    Add cancel_url to checkout session creation

--- ecommerce-admin/app/api/[storeId]/checkout/route.ts
+++ ecommerce-admin/app/api/[storeId]/checkout/route.ts
@@ -76,6 +76,7 @@ export async function POST(
     phone_number_collection: {
       enabled: true
     },
-    success_url: '${process.env.FRONTEND_STORE_URL}/cart?success=1'
+    success_url: '${process.env.FRONTEND_STORE_URL}/cart?success=1',
+    cancel_url: '${process.env.FRONTEND_STORE_URL}/cart?canceled=1',
   });
 }
