a12094446d129e88830d1f393d6afcd61aa297b2
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-21 21:37:39 -0800

    Get signature in webhook endpoint to verify event

--- ecommerce-admin/app/api/webhook/route.ts
+++ ecommerce-admin/app/api/webhook/route.ts
@@ -1,4 +1,5 @@
 import Stripe from 'stripe';
+import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 import { stripe } from '@/lib/stripe';
@@ -6,4 +7,5 @@ import prismadb from '@/lib/prismadb';
 
 export async function POST(req: Request) {
   const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
 }
