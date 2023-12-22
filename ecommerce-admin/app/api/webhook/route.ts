eb9bb6e91d44332531c6b1f9360b6b54aa038c41
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-21 21:24:34 -0800

    Add POST request to webhook endpoint

--- ecommerce-admin/app/api/webhook/route.ts
+++ ecommerce-admin/app/api/webhook/route.ts
@@ -4,3 +4,6 @@ import { NextResponse } from 'next/server';
 import { stripe } from '@/lib/stripe';
 import prismadb from '@/lib/prismadb';
 
+export async function POST(req: Request) {
+  const body = await req.text();
+}
