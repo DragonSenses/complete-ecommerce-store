61a56528d8afaf86668d85ec3a514ecdf0597b66
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-30 14:24:05 -0800

    Add Card component to dashboard page

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -5,6 +5,15 @@ import React from 'react';
 import prismadb from '@/lib/prismadb';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { DollarSign } from 'lucide-react';
 
 interface DashboardPageProps {
   params: { storeId: string }
@@ -19,7 +28,16 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
       <div className='flex-1 space-y-4 p-8 pt-6'>
         <Heading title='Dashboard' description='Overview of your store' />
         <Separator />
-        
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+              <CardTitle className='text-sm font-medium'>
+                Total Revenue
+              </CardTitle>
+              <DollarSign />
+            </CardHeader>
+          </Card>
+        </div>
       </div>
     </div>
   );
