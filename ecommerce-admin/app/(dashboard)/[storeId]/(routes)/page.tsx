30c384ec72f8b1ad008e0ed8ef8633d8bd4de350
DragonSenses <kendrickfrancisco@gmail.com>
2023-12-30 14:30:37 -0800

    Add total revenue card to dashboard page
    Use priceFormatter to format the revenue amount and display it in a CardContent component inside a Card component. Add DollarSign icon from lucide-react to the CardHeader.

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,6 @@
 // Global Imports
 import React from 'react';
+import { DollarSign } from 'lucide-react';
 
 // Local Imports
 import prismadb from '@/lib/prismadb';
@@ -13,7 +14,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { DollarSign } from 'lucide-react';
+import { priceFormatter } from '@/lib/utils';
 
 interface DashboardPageProps {
   params: { storeId: string }
@@ -28,16 +29,25 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
       <div className='flex-1 space-y-4 p-8 pt-6'>
         <Heading title='Dashboard' description='Overview of your store' />
         <Separator />
+        
         <div className="grid gap-4 grid-cols-3">
+
           <Card>
             <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
               <CardTitle className='text-sm font-medium'>
                 Total Revenue
               </CardTitle>
-              <DollarSign />
+              <DollarSign className='h-4 w-4 text-muted-foreground' />
             </CardHeader>
+            <CardContent>
+              <div className='text-2xl font-bold'>
+                {priceFormatter.format(100)}
+              </div>
+            </CardContent>
           </Card>
+
         </div>
+
       </div>
     </div>
   );
