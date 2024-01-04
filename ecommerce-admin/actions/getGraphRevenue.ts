eae84d5e7e8aa2cca6bd008f94220ae26731d453
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-04 12:36:08 -0800

    Define graph data to display revenue

--- ecommerce-admin/actions/getGraphRevenue.ts
+++ ecommerce-admin/actions/getGraphRevenue.ts
@@ -1,5 +1,10 @@
 import prismadb from "@/lib/prismadb";
 
+interface GraphData {
+  name: string;
+  total: number;
+}
+
 /**
  * Get monthly revenue data for a given store
  * @param storeId - The ID of the store to query
@@ -36,9 +41,29 @@ export default async function getGraphRevenue(storeId: string) {
       revenueForOrder += item.product.price.toNumber();
     }
 
-    // Add the revenue for this order to the corresponding month in the monthly
-    // revenue object. If its not available by default then use 0 as the default value.
-    // Add the revenue from the order.
+    /* Add the revenue for this order to the corresponding month in the monthly 
+    revenue object. If data for that month is not available then use 0 as 
+    the default value. Add the revenue from the order. 
+    */
     monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder;
   }
-};
+
+  const graphData: GraphData[] = [
+    {name: "Jan", total: 0  },
+    {name: "Feb", total: 0  },
+    {name: "Mar", total: 0  },
+    {name: "Apr", total: 0  },
+    {name: "May", total: 0  },
+    {name: "Jun", total: 0  },
+    {name: "Jul", total: 0  },
+    {name: "Aug", total: 0  },
+    {name: "Sep", total: 0  },
+    {name: "Oct", total: 0  },
+    {name: "Nov", total: 0  },
+    {name: "Dec", total: 0  },
+  ];
+
+  for (const month in monthlyRevenue) {
+    graphData[parseInt(month)]
+  }
+}
