3b1eb7e8dee2c4126bb3fdbdb049f5d62ec3ce16
DragonSenses <kendrickfrancisco@gmail.com>
2024-01-12 17:08:09 -0800

    Update headers in next config to configure CORS

--- ecommerce-admin/next.config.js
+++ ecommerce-admin/next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/checkout",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
