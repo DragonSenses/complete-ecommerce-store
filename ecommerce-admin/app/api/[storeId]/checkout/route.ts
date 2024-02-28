44f6f6ebe91d425ef429a20c424f05775acad936
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-27 17:18:26 -0800

    feat: Simplify CORS and fix URLs in checkout
    In the checkout API route, remove CORS middleware and configure CORS policy by simply setting HTTP response headers.
    
    Also within the checkout session, update the success_url and cancel_url by using template strings. This ensures that the URLs dynamically incorporate the FRONTEND_STORE_URL environment variable.

--- ecommerce-admin/app/api/[storeId]/checkout/route.ts
+++ ecommerce-admin/app/api/[storeId]/checkout/route.ts
@@ -1,12 +1,8 @@
 import Stripe from 'stripe';
-import { NextRequest, NextResponse } from 'next/server';
-
-import NextCors from 'nextjs-cors';
-var cors = require('cors');
+import { NextResponse } from 'next/server';
 
 import { stripe } from '@/lib/stripe';
 import prismadb from '@/lib/prismadb';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 // Configure the CORS policy by setting HTTP response headers
 const corsHeaders = {
@@ -15,45 +11,14 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-// Initialize the cors middleware
-const corsMiddleware = cors({
-  origin: "http://localhost:3001", // The origin of frontend app
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // The HTTP methods to allow
-  allowedHeaders: ['Content-Type', 'Authorization'], // The headers to allow
-  optionsSuccessStatus: 200 // The status code to send for OPTIONS requests
-});
-
-// Wrap the cors middleware in a promise
-const runCors = (req: Request, res: Response) => {
-  return new Promise((resolve, reject) => {
-    corsMiddleware(req, res, (result: Error) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-};
-
 export async function OPTIONS() {
   return NextResponse.json({}, { headers: corsHeaders });
 }
 
 export async function POST(
-  req: NextApiRequest,
-  res: NextApiResponse,
+  req: Request,
   { params }: { params: { storeId: string } }
 ) {
-  // Run the cors middleware before handling the request
-  // await runCors(req, res);
-
-  // Run NextCors before handling the request
-  await NextCors(req, res, {
-    origin: "http://localhost:3001", // The origin of frontend app
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // The HTTP methods to allow
-    allowedHeaders: ['Content-Type', 'Authorization'], // The headers to allow
-    optionsSuccessStatus: 200 // The status code to send for OPTIONS requests
-  });
 
   // Check if storeId exists
   if (!params.storeId) {
@@ -112,16 +77,16 @@ export async function POST(
   // Use line items to create the checkout session using Stripe API
   const session = await stripe.checkout.sessions.create({
     line_items,
-    mode: "payment",
-    billing_address_collection: "required",
+    mode: 'payment',
+    billing_address_collection: 'required',
     phone_number_collection: {
-      enabled: true
+      enabled: true,
     },
-    success_url: '${process.env.FRONTEND_STORE_URL}/cart?success=1',
-    cancel_url: '${process.env.FRONTEND_STORE_URL}/cart?canceled=1',
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
     metadata: {
       orderId: order.id
-    }
+    },
   });
 
   return NextResponse.json({ url: session.url }, { headers: corsHeaders });
