fa311edbdba7cf74b689cd6077d2e8d5c2d60222
DragonSenses <kendrickfrancisco@gmail.com>
2024-02-27 21:38:57 -0800

    feat: Disable checkout button when cart is empty

--- ecommerce-store/app/(routes)/cart/components/Summary.tsx
+++ ecommerce-store/app/(routes)/cart/components/Summary.tsx
@@ -64,7 +64,11 @@ const Summary = () => {
         </div>
       </div>
 
-      <Button onClick={onCheckout} className='w-full mt-6'>
+      <Button 
+        disabled={items.length === 0}
+        onClick={onCheckout} 
+        className='w-full mt-6'
+      >
         Checkout
       </Button>
     </div>
